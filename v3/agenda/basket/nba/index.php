d33f67b5a9462e13c9beb6996b466bfccbeefb17
Alex <slowdsports@gmail.com>
2022-08-21 05:06:13 -0600

    basknba

--- v3/agenda/basket/nba/index.php
+++ v3/agenda/basket/nba/index.php
@@ -2,6 +2,8 @@
 session_start();
 $_SESSION['referer'] = "//" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 include('../../../inc/header.php'); include('../../../conn.php'); include('../../../inc/scraper.php');
+$query=mysqli_query($conn,"select * from user where userid='".$_SESSION['id']."'");
+$row=mysqli_fetch_assoc($query);
 
 $base ="http://givemenbastreams.com/nba.php?g=";
 $g = $_GET['g'];
@@ -12,18 +14,13 @@ preg_match("(source\:\s'(.*?)')", $site, $stream);
 
 if(isset($_GET['g'])){
     if ($stream[1] == null){
-        echo "No hay Stream ";
-    }
-
-    else{
+        $_SESSION['nbaError'] = "No se ha podido cargar la retransmisión del juego.";
+    } else{
         $m3u8 = base64_encode($stream[1]);
         // header ("location: ../../play?nba&plyr=vid&c=$m3u8");
         echo '<script>window.location.href = "../../play?nba&plyr=vid&c='.$m3u8.'";</script>';
     }
 }
-
-$query=mysqli_query($conn,"select * from user where userid='".$_SESSION['id']."'");
-$row=mysqli_fetch_assoc($query);
 ?>
 
 <!-- App Capsule -->
@@ -48,10 +45,25 @@ if (isset($_SESSION['message']) ){
     <h4 class="subtitle">¡Disfruta de tu evento!</h4>
 </div>
 
+<!-- Alerta -->
+<?php
+if (isset($_SESSION['nbaError'])){
+?>
+<br>
+<div class="container">
+    <div class="alert alert-danger text-center" role="alert">
+    <?=$_SESSION['nbaError'];?>
+    </div>
+</div>
+<br>
+<?php unset($_SESSION['nbaError']); }?>
+<!-- *Alerta -->
+
 <!-- Categorías -->
     <div class="section mt-2">
         <div class="row">
             <?php
+            // get DOM from URL or file
             $html = file_get_html('https://www.espn.com.mx/basquetbol/nba/calendario');
             $fecha = $html -> find ('div.ResponsiveTable');
             $juegos = $html ->find ('tbody tr');
@@ -67,42 +79,48 @@ if (isset($_SESSION['message']) ){
                     $finalizado = $juego ->find ('td.teams__col a.AnchorLink',0)-> plaintext;
                     $time = "Finalizó";
                 }
+
                 if ($time == "EN VIVO"){
                     $time = '<i class="fas fa-circle faa-flash animated"></i> En Vivo';
                     $finalizado = "";
                 }
-            // Teams
-            include ('teams.php');
+                // Teams
+                include ('teams.php');
             ?>
             <!-- Elemento -->
             <div class="col-12 mycard">
-                <a data-toggle="collapse" href="#juego<?=$local.$visita?>" role="button" aria-expanded="false" aria-controls="juego<?=$local.$visita?>">
+                <a data-toggle="collapse" href="#juego-<?=$local?>-<?=$visita?>" role="button" aria-expanded="false" aria-controls="juego-<?=$local?>-<?=$visita?>">
                     <div class="card product-card">
-                        <div class="card-body">
-                            <center>
-                                <img width="48px" src="<?=$app?>assets/img/ligas/<?=$result['ligaImg']?>.png" alt="" class="float-left">
-                                <img width="48px" src="https://cdn.worldvectorlogo.com/logos/movistar-plus-2.svg" alt="" class="float-right">
-                                <div class="row">
-                                    <div class="col-6">
-                                        <img src="<?=$result['logoL']?>" alt="">
-                                        <h4><?=ucfirst($local)?></h4>
-                                    </div>
-                                    <div class="col-6">
-                                        <img src="<?=$result['logoV']?>" alt="">
-                                        <h4><?=ucfirst($visita)?></h4>
-                                    </div>
+                        <div class="main-event">
+                            <div class="league">
+                                <img src="<?=$app?>assets/img/ligas/nba.png" alt="League" />
+                            </div>
+                            <div class="match">
+                                <div class="team">
+                                    <img width="60px" src="<?=$app?>assets/img/equipos/nba/<?=$local?>.png" alt="<?=$local?>" />
+                                    <h4><?=ucfirst($local)?></h4>
                                 </div>
-                            </center>
+                                <div class="vs">
+                                    <h6>vs</h6>
+                                </div>
+                                <div class="team">
+                                    <img width="60px" src="<?=$app?>assets/img/equipos/nba/<?=$visita?>.png" alt="<?=$visita?>" />
+                                    <h4><?=ucfirst($visita)?></h4>
+                                </div>
+                            </div>
+                            <div class="channel">
+                                <img src="http://iraffle.live/v3/assets/img/ligas/uel.png" alt="Channel" />
+                            </div>
                         </div>
                     </div>
                 </a>
-                <div class="collapse" id="juego<?=$result['id']?>">
+                <div class="collapse" id="juego-<?=$local?>-<?=$visita?>">
                     <div class="card card-body">
                         <ul class="listview link-listview">
                             <li>
-                                <a class="justify-content-center" href="../../play/?c=<?=$row['channelId']?>">
-                                    <i class="flag <?=$row['countryImg']?>"></i>
-                                    <?=$row['channelName']?>
+                                <a class="justify-content-center" href="?g=<?=$local?>">
+                                <i class="flag us"></i>
+                                NBA TV - <?=ucfirst($local)?> | HD
                                 </a>
                             </li>
                         </ul>
@@ -110,11 +128,11 @@ if (isset($_SESSION['message']) ){
                 </div>
             </div>
             <!-- End Elemento -->
+            <?php endforeach; ?>
 
         </div>
     </div>
 <!-- End Categorías -->
 <?php
-endforeach;
 include('../../../inc/navbar.php');
 ?>
