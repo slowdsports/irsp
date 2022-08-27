51e28331b56ed91fd561a088d4ba4460f493c82b
Alex <slowdsports@gmail.com>
2022-08-27 12:22:56 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -3,7 +3,7 @@ if ($local == "milan"){
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9DcmVtb25lc2VfdnNfVG9yaW5vXzIwMjIwODI3XzE2NjE2MTE4MjMwMTkvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvRkI1MTE3NjE1Mzc0QjM2RkIwNzY0NzNGNUExREJCQkE1NjZENjBFNTQwQzBCRkU3RkU5RERERDE3OUFDREQ0Ny9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTQzNzRiMDIwMTdiNDRkZmU5MDE1ODFjZjk3ZmMyNDY0JmtleTI9ZjExODkwNWNjM2I3MmM4ZWNjOGRmYzc4MmZkMTA5NGM=">
-        <i class="flag es"></i>
+        <i class="flag ar"></i>
         Star + | HD
         </a>
     </li>
@@ -13,7 +13,7 @@ if ($local == "spezia"){
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9DcmVtb25lc2VfdnNfVG9yaW5vXzIwMjIwODI3XzE2NjE2MTE4MjMwMTkvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvRkI1MTE3NjE1Mzc0QjM2RkIwNzY0NzNGNUExREJCQkE1NjZENjBFNTQwQzBCRkU3RkU5RERERDE3OUFDREQ0Ny9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTQzNzRiMDIwMTdiNDRkZmU5MDE1ODFjZjk3ZmMyNDY0JmtleTI9ZjExODkwNWNjM2I3MmM4ZWNjOGRmYzc4MmZkMTA5NGM=">
-        <i class="flag es"></i>
+        <i class="flag ar"></i>
         Star + | HD
         </a>
     </li>
