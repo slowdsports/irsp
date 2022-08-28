8904b6abcce3a4cd8eb569492038c24137e3477e
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-08-28 10:20:56 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -2,7 +2,7 @@
 if ($result['visita'] == "tottenham"){
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="javascript:void(0)">
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9Ob3R0aW5naGFtX0ZvcmVzdF92c19Ub3R0XzIwMjIwODI4XzE2NjE2OTQ2MDcwMjAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNTRBMEQ2RjAzNUE4REVCRUFDMjg0RTkwQjk0NkI2QzNBMTAyM0VEQjI4MkM1MzgzOTI2ODA1ODA1NUM3Q0ZGMC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTEyYzNkOTc1NjRiNDRlMWRiN2EwYTU2MjYzMGIwM2FjJmtleTI9M2Q1NDdmYjIwZDQ5OTNjODliNjVhOTMxNDZjOGE5NzU=">
         <i class="flag ar"></i>
         Star + | HD
         </a>
@@ -12,7 +12,7 @@ if ($result['visita'] == "tottenham"){
 if ($result['visita'] == "villarreal"){
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="javascript:void(0)">
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9HZXRhZmVfdnNfVmlsbGFycmVhbF8yMDIyMDgyOF8xNjYxNjk0NjE4MDEzL2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzNGQTVDNzA3MUZGMEE0RkVBNTYzRTczOTREMUVEN0FGRUU5RjQ5OTNFRURBRjBGRUE4NzE5Q0NFOTNBNTdEQ0Uvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT1iOGJjMDU2YjM4ZDA0ZDcyYjFkNjVkMzFjMzkzNzgwYiZrZXkyPTBlYjBkYWJjNTJhNjM2MmM0NjAxZTE2ODg3Yzc0Zjg0">
         <i class="flag ar"></i>
         Star + | HD
         </a>
