1a6de174b32fee622c76da3b535f1a5f04c4a89e
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-08-28 13:17:23 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -42,7 +42,7 @@ if ($result['visita'] == "sampdoria"){
 if ($result['visita'] == "monaco"){
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="javascript:void(0)">
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9QU0dfdnNfTW9uYWNvXzIwMjIwODI4XzE2NjE3MDYwMDA1NzYvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNDc5NkFBRjJGMjJCNDFDOTREMTNGNjc5NjkxNTBDNTFCMDRDRDhFNEM0MjNFMUYwQUJGQkFDNzc1NUM0QjU3NC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTFiZDIzMTQ4NzMyNzQ5NWY5MzY3MzI2OGU4YzBjNWM0JmtleTI9MjIzYjUzOGYwNDljZWU1NzZmMmQ0NWUxMmQwMjJjNTY=">
         <i class="flag ar"></i>
         Star + | HD
         </a>
@@ -52,7 +52,7 @@ if ($result['visita'] == "monaco"){
 if ($result['visita'] == "napoli"){
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="javascript:void(0)">
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9GaW9yZW50aW5hX3ZzX05hcG9saV8yMDIyMDgyOF8xNjYxNzA2MDQ2MDI2L2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzA5OUYxOTRCNTQ0MTU1Q0UwNjZEM0U3NDc5NTRBRDE1OEI3MTA4RkZFRDhCNTk2N0I1QzBCRjkwNEY4OUI0NjEvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT05N2MzNjQxNDgxZWI0NGI3YjA2ZTZiZGRkZjdiM2I4ZSZrZXkyPWM2NzY4YmI2MDQzMGZmMTJlY2FhNTAyMzFiNTI1YzNm">
         <i class="flag ar"></i>
         Star + | HD
         </a>
@@ -62,7 +62,7 @@ if ($result['visita'] == "napoli"){
 if ($result['visita'] == "empoli"){
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="javascript:void(0)">
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9MZWNjZV92c19FbXBvbGlfMjAyMjA4MjhfMTY2MTcwNjA0OTAxMy9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci9ENDBEM0E4QjZCQTRCM0UxM0Q0NDFFMEYyMzFENDhCRTZFRDYyNENFODdFMDY2NDE1MDY3MEVFQjEzRTIyQTUzL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YzdmZjYwYWE1ZjMxNDRiM2JjODdmOTBhNTZiZGVhNzcma2V5Mj1hZTZjMjViNTE5MmM3ZmFlN2Q0NjU5MDY2ZmI4NmJiOQ==">
         <i class="flag ar"></i>
         Star + | HD
         </a>
