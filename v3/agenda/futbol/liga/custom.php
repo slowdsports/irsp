1b0d9ac9e28605220dbcd127d79225938b165f2f
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-08-28 08:44:41 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -1,5 +1,75 @@
 <?php
-if ($local == "milan"){
+if ($result['visita'] == "tottenham"){
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "villarreal"){
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "atalanta"){
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "sampdoria"){
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "monaco"){
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "napoli"){
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "empoli"){
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['local'] == "espanyol"){
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r=Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yNy9NaWxhbl92c19Cb2xvZ25hXzIwMjIwODI3XzE2NjE2MTk2MTIwMzcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQTZERjlFMDM5MjQxNzdEM0QyRkE0NEJEMkYzNzg5MUIxM0FCNkQ5QjIzMTMwNUQxOEExQ0M2MEYwMzYzNzlGOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTAwZjlmYTZjNGM4MDRiNDg4YjIxMzg5M2NmNDY2YTZmJmtleTI9ZWI2MGU4ODRmNjUzNGU5ZWJkYTExN2U3YTEzZDk5M2I=">
