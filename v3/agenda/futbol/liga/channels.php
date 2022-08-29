68b2b6e0f461f9699ca69f7c318bd4c3edc38c9d
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-08-29 07:23:24 -0600

    Update channels.php

--- v3/agenda/futbol/liga/channels.php
+++ v3/agenda/futbol/liga/channels.php
@@ -567,6 +567,15 @@ if($canal === "foxsports3sur" || $canal === "foxsports3sur"){
         FOX Sports 3 | HD
         </a>
     </li>';
+} elseif($canal2 === "goltvec" || $canal2 === "goltvec"){
+    $canalImg = "goltv";
+    $canal2 = '
+    <li>
+        <a class="justify-content-center" href="https://irpc.ga/mg/play/mlaliga.php?get=aHR0cHM6Ly8xMTM0LXZvcy5kdHZvdHQuY29tL0RBU0gvbWFuaWZlc3QubXBk&key=MzMwM2I0YzY5ODJmMzgxZWE5ZTMwOTAwOWZiYjNmMjc=&key2=YTdmZDQ5ODA4Zjg5MGMyNTAwYmYyMTgwYWFmNDIzMDc=">
+        <i class="flag ec"></i>
+        Gol TV | HD
+        </a>
+    </li>';
 }
 
 if ($canalop2 == $canalop3){
