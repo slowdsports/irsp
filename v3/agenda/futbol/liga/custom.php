8ee54f34ea6a8adb17c2ae04ef74cb171abda761
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-08-31 10:28:03 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -115,6 +115,19 @@ if ($result['local'] === "liverpool"){
     </li>
     ';
 }
+if ($result['local'] === "toulouse"){
+    $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
 if ($result['visita'] === "tottenham"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
