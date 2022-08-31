5ecf48f2de8df9be7141708ae20703cf64a19fcc
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-08-31 09:18:54 -0600

    fdfd

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -71,3 +71,63 @@ if ($result['visita'] == "lecce"){
     </li>
     ';
 }
+if ($result['local'] == "arsenal"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9Ob3R0aW5naGFtX0ZvcmVzdF92c19Ub3R0XzIwMjIwODI4XzE2NjE2OTQ2MDcwMjAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNTRBMEQ2RjAzNUE4REVCRUFDMjg0RTkwQjk0NkI2QzNBMTAyM0VEQjI4MkM1MzgzOTI2ODA1ODA1NUM3Q0ZGMC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTEyYzNkOTc1NjRiNDRlMWRiN2EwYTU2MjYzMGIwM2FjJmtleTI9M2Q1NDdmYjIwZDQ5OTNjODliNjVhOTMxNDZjOGE5NzU=";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['local'] == "bournemouth"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9Ob3R0aW5naGFtX0ZvcmVzdF92c19Ub3R0XzIwMjIwODI4XzE2NjE2OTQ2MDcwMjAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNTRBMEQ2RjAzNUE4REVCRUFDMjg0RTkwQjk0NkI2QzNBMTAyM0VEQjI4MkM1MzgzOTI2ODA1ODA1NUM3Q0ZGMC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTEyYzNkOTc1NjRiNDRlMWRiN2EwYTU2MjYzMGIwM2FjJmtleTI9M2Q1NDdmYjIwZDQ5OTNjODliNjVhOTMxNDZjOGE5NzU=";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['local'] == "liverpool"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9Ob3R0aW5naGFtX0ZvcmVzdF92c19Ub3R0XzIwMjIwODI4XzE2NjE2OTQ2MDcwMjAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNTRBMEQ2RjAzNUE4REVCRUFDMjg0RTkwQjk0NkI2QzNBMTAyM0VEQjI4MkM1MzgzOTI2ODA1ODA1NUM3Q0ZGMC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTEyYzNkOTc1NjRiNDRlMWRiN2EwYTU2MjYzMGIwM2FjJmtleTI9M2Q1NDdmYjIwZDQ5OTNjODliNjVhOTMxNDZjOGE5NzU=";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "tottenham"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9Ob3R0aW5naGFtX0ZvcmVzdF92c19Ub3R0XzIwMjIwODI4XzE2NjE2OTQ2MDcwMjAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNTRBMEQ2RjAzNUE4REVCRUFDMjg0RTkwQjk0NkI2QzNBMTAyM0VEQjI4MkM1MzgzOTI2ODA1ODA1NUM3Q0ZGMC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTEyYzNkOTc1NjRiNDRlMWRiN2EwYTU2MjYzMGIwM2FjJmtleTI9M2Q1NDdmYjIwZDQ5OTNjODliNjVhOTMxNDZjOGE5NzU=";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['visita'] == "bayern"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOC8yOC9Ob3R0aW5naGFtX0ZvcmVzdF92c19Ub3R0XzIwMjIwODI4XzE2NjE2OTQ2MDcwMjAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNTRBMEQ2RjAzNUE4REVCRUFDMjg0RTkwQjk0NkI2QzNBMTAyM0VEQjI4MkM1MzgzOTI2ODA1ODA1NUM3Q0ZGMC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTEyYzNkOTc1NjRiNDRlMWRiN2EwYTU2MjYzMGIwM2FjJmtleTI9M2Q1NDdmYjIwZDQ5OTNjODliNjVhOTMxNDZjOGE5NzU=";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
