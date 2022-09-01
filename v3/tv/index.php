75be30fc824d4dd7f19e470cf5a6739208fb5697
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-09-01 08:16:27 -0600

    Update index.php

--- v3/tv/index.php
+++ v3/tv/index.php
@@ -116,9 +116,7 @@ if (isset($_SESSION['message']) ){
                     echo "display: none";
                 };
                 ?>" class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 mycard <?=$result['categoryName']?>">
-                    <a href="<?php if ($result['type'] == 9){
-                        echo "https://irafflevip.tk/tv/";
-                        } ?>epg?url=<?=$result['epg']?>&c=<?=$result['channelId']?>">
+                    <a href="epg?url=<?=$result['epg']?>&c=<?=$result['channelId']?>">
                         <div class="card product-card liga-card">
                             <div class="card-body">
                                 <center>
