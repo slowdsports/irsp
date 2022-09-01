44f686ed9f0f1cc46f2a893f71edb75d553ce1b2
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-09-01 07:54:07 -0600

    Update valid.php

--- v3/tv/epg/valid.php
+++ v3/tv/epg/valid.php
@@ -132,8 +132,8 @@ elseif ($typeChannel == 7){
 } // Megatv
 //elseif ($_GET['get'] || $_GET['key'] || $GET['key2']){
 elseif ($typeChannel == 9){
-    //if($_SERVER['SERVER_PORT'] != '443') { header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']); exit(); }
-    if($_SERVER['SERVER_PORT'] != '443') { header('Location: https://irafflevip.tk/'.$_SERVER['REQUEST_URI']); exit(); }
+    if($_SERVER['SERVER_PORT'] != '443') { header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']); exit(); }
+    //if($_SERVER['SERVER_PORT'] != '443') { header('Location: https://irafflevip.tk'.$_SERVER['REQUEST_URI']); exit(); }
     $core = '<script src="mg-jwp.js"></script>';
     $getURL = base64_encode($row['channelUrl']);
     $getKEY = $row['key1'];
