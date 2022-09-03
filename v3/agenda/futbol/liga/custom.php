2a71ec326da491a823055218d56d795f83743188
Eduardo Perdomo <33327508+eduveel19@users.noreply.github.com>
2022-09-03 10:31:34 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -11,10 +11,10 @@ if ($result['visita'] === "team"){
     </li>
     ';
 }
-if ($result['visita'] === "juventus"){
+if ($result['visita'] === "atletico"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wMS9BdGFsYW50YV92c19Ub3Jpbm9fMjAyMjA5MDFfMTY2MjA1MTYyNDAyMC9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci80NjFGMDMyRkQyODlBNTY5MUIzNzY0NkE0NkU1ODAxMEIxRUVCNTVDMzE1QUVCOTQ5QzJFRTlBNEMwMzEzMDBGL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YTM5YmNiNjliMzNlNDc5NDk4ZjkzMGY5YTk1MjVhNzIma2V5Mj0wZmVmNDZhNzE1MThmMjI2ZGVlNmNmZGJlNDcxZjQ2MA==";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wMy9SZWFsX1NvY2llZGFkX3ZzX0F0bHRpY29fXzIwMjIwOTAzXzE2NjIyMTYzMzIwMjcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvMzc1RjdGQjA3Qjc5RjZEMzZGRUQ4ODA1ODlFOTQwMUM1NDUwNUE3NzMwOTdEQzdEMkMyRThEQzQ3MjM2MzcyMC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTgxNzk2MGRiNDQ3NzQzMTA4MzFkMmRmMDc0NGVmOTRhJmtleTI9OGQ1OGE5ZTNlMWQ4Njg5ODY2NTkwOWE1NTI4OWU3MWY=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
