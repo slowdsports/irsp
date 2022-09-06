539a7cae50433cffd1dbf65f5cf618fe9358c00c
slowdsports <72911530+slowdsports@users.noreply.github.com>
2022-09-06 12:48:12 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -40,7 +40,7 @@ if ($result['local'] === "dortmund"){
 if ($result['local'] === "psg"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9QU0dfdnNfSnV2ZW50dXNfRkNfMjAyMjA5MDZfMTY2MjQ4NDIxMDA0Ni9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci83QTQ5NzQzMkRCMjUyOUQ4RUE1RTQ0QTQ1NDdDODI0NTg4MUZDMEQ4MDg4MzUxRUVBQjMzQkJEMjEzQzc0MDUzL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9ZDI2MDdhYWQ1NWQxNDcwNjk2OGRlZmM1MzUzMDI5NjIma2V5Mj04ZTc1OWQ1MjA1NDFjNDFlOGQ3ZGMyYjA5NjdkMzQzMg==";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -53,7 +53,20 @@ if ($result['local'] === "psg"){
 if ($result['local'] === "celtic"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9DZWx0aWNfRkNfdnNfUmVhbF9NYWRyaWRfMjAyMjA5MDZfMTY2MjQ4NDIzMzAxOC9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci80QTI1QjM3MzlFRjE1QkVGQzBEMjk4NUFENzY4ODVCMTA1NEFBM0M1RjMwRTJGMkJGQUZGMzA2QjBCNkU3NTdBL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YmU5ZWMyYzMyZmVjNGJkODkwYzRmN2MyNTY4NzZiNTUma2V5Mj1iYzZmZGRmZDE0YWIwYTIwMmMzMWM1NjI0MmE1ZmJkOQ==";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['local'] === "sevilla"){
+    $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9TZXZpbGxhX3ZzX01hbmNoZXN0ZXJfQ2l0XzIwMjIwOTA2XzE2NjI0ODQyMzMwNDEvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvOEM5MzQ3Mzg5ODcwQTQxRUE2NUQxMzBDMTFCREEyNzI0NDBENkQ5ODBBMjA5QTgxQ0JCQkI1QUQyMTgxM0VCNi9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTc4MTQwMTcyMDFlMTRjNjBiNzJiMTE2MTZhNTcwMDA0JmtleTI9MzIwNDIxMDgxNmQ1ZDhkMzJlNzhiNWE4YTJiYzllYmY=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
