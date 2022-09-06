0ede3c5cda63487db6152eb018df78d468130ed2
Eduardo Perdomo <33327508+eduveel19@users.noreply.github.com>
2022-09-06 10:50:52 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -14,7 +14,7 @@ if ($result['visita'] === "team"){
 if ($result['local'] === "zagreb"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9EaW5hbW9fWmFncmViX3ZzX0NoZWxzZWFfMjAyMjA5MDZfMTY2MjQ3NjQyMDA3NC9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci8yNEE3MzUyNzFGRjFFMjBBOUQ0MTM4QUQ5QTAyQUVEMDMyMUU5RjJEQjYwRDczOTg5MUJFMzZCRDQ4RTgxNEJFL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YTBlYTIyMDQ0MzczNGExMWIxZjNkNWQyNTc4MDM0OTMma2V5Mj0yYjBlZTIwNGFjMWEyZThiOWQxZjcyZTk3ZGY0OTY4Nw==";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -27,7 +27,7 @@ if ($result['local'] === "zagreb"){
 if ($result['local'] === "dortmund"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9Cb3J1c3NpYV9Eb3J0bXVuZF92c19Db3BlXzIwMjIwOTA2XzE2NjI0NzY0MDgwMzEvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvN0RDRUMxRjUzRUJFNjFERTRDMUVDNkNCNjU3MDNDNUFEMTlFN0M0MENDMjg4MDFBRDRBRjhGMEMyQjBDMjE1RS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTY5Y2IzZTQ0MjQwYTQ1ZjM4YThiZjc2ZTFhZjgwOGU4JmtleTI9MzFiODg0YTE0YmFjNTg2MmFmNTc0MWU2MDBhMTk3ZGE=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
