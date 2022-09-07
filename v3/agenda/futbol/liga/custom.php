42ed7ad3049534495cb31ec4fea226c57e31d32e
Eduardo Perdomo <33327508+eduveel19@users.noreply.github.com>
2022-09-07 12:56:34 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -14,7 +14,7 @@ if ($result['visita'] === "team"){
 if ($result['local'] === "barcelona"){
     $_SESSION['campeones'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "campeones";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9EaW5hbW9fWmFncmViX3ZzX0NoZWxzZWFfMjAyMjA5MDZfMTY2MjQ3NjQyMDA3NC9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci8yNEE3MzUyNzFGRjFFMjBBOUQ0MTM4QUQ5QTAyQUVEMDMyMUU5RjJEQjYwRDczOTg5MUJFMzZCRDQ4RTgxNEJFL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YTBlYTIyMDQ0MzczNGExMWIxZjNkNWQyNTc4MDM0OTMma2V5Mj0yYjBlZTIwNGFjMWEyZThiOWQxZjcyZTk3ZGY0OTY4Nw==";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNy9GQ19CYXJjZWxvbmFfdnNfRkNfVmlrdG9yXzIwMjIwOTA3XzE2NjI1NzA2NTIwMTcvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvRUI2NjgxQUY4ODI3MjQzN0U1MTE5NDg2MEI3NTEzRjJFRTI1N0VEREU3QUQ5RTgwMDUwQjUwQzE4MjM3MUNGRC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTQwNTY1NDg3ZTExYzQ2OTY5NjM4NmRmNmFmYTc0NzUxJmtleTI9NWY0NTkwOGI0ZmU2NzgzYWU0NGVmYTFkN2Q4Zjg4MjI=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -24,10 +24,10 @@ if ($result['local'] === "barcelona"){
     </li>
     ';
 }
-if ($result['local'] === "dortmund"){
+if ($result['local'] === "at.madrid"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9Cb3J1c3NpYV9Eb3J0bXVuZF92c19Db3BlXzIwMjIwOTA2XzE2NjI0NzY0MDgwMzEvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvN0RDRUMxRjUzRUJFNjFERTRDMUVDNkNCNjU3MDNDNUFEMTlFN0M0MENDMjg4MDFBRDRBRjhGMEMyQjBDMjE1RS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTY5Y2IzZTQ0MjQwYTQ1ZjM4YThiZjc2ZTFhZjgwOGU4JmtleTI9MzFiODg0YTE0YmFjNTg2MmFmNTc0MWU2MDBhMTk3ZGE=";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNy9BdGx0aWNvX01hZHJpZF92c19GQ19Qb3J0XzIwMjIwOTA3XzE2NjI1NzA2MjEwMTYvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvN0U2M0UwMEY4MzNFOEQ0N0Y3MkJDRUY2QUQ5MUI2MTg1NDdBRTUyN0ZEMEJGMkFBQkFCNkM1RjgxNDZBNTQ5Qi9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PWVlMmE1MTQwMTRiMTRkYWJhYTM2ZTQyYjQzMjMzMDE5JmtleTI9ZDRiZTk2OTJiZjdmMWNlYWY3MTBmM2RjNzI5YzYwZWM=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -37,10 +37,10 @@ if ($result['local'] === "dortmund"){
     </li>
     ';
 }
-if ($result['local'] === "psg"){
+if ($result['local'] === "inter"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9QU0dfdnNfSnV2ZW50dXNfRkNfMjAyMjA5MDZfMTY2MjQ4NDIxMDA0Ni9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci83QTQ5NzQzMkRCMjUyOUQ4RUE1RTQ0QTQ1NDdDODI0NTg4MUZDMEQ4MDg4MzUxRUVBQjMzQkJEMjEzQzc0MDUzL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9ZDI2MDdhYWQ1NWQxNDcwNjk2OGRlZmM1MzUzMDI5NjIma2V5Mj04ZTc1OWQ1MjA1NDFjNDFlOGQ3ZGMyYjA5NjdkMzQzMg==";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNy9JbnRlcl9NaWxhbl92c19CYXllcm5fTXVuXzIwMjIwOTA3XzE2NjI1NzA2MjIwMzYvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvNjM1ODhFQ0QxNTE4NUI1ODc0NDk0NThEQjZGMTBGREM3MkQ3OUU4NzNFNzdGQTc0ODZDNEEzMDBENTc4MTBEQi9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PWQ5ZGNiZDdhOGVmNDRmOTdhY2FjYTg3ZTMxYjA4YWRhJmtleTI9NDk0YWJlMWQ0MzBjNzk3MmM5NzY3NzU2N2U4YWQ1ZmM=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -50,10 +50,10 @@ if ($result['local'] === "psg"){
     </li>
     ';
 }
-if ($result['local'] === "celtic"){
+if ($result['local'] === "napoli"){
     $_SESSION['starplus'] = "Los eventos por Star+ estarán listos 5 o 10 minutos antes del comienzo.";
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDIvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNi9DZWx0aWNfRkNfdnNfUmVhbF9NYWRyaWRfMjAyMjA5MDZfMTY2MjQ4NDIzMzAxOC9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci80QTI1QjM3MzlFRjE1QkVGQzBEMjk4NUFENzY4ODVCMTA1NEFBM0M1RjMwRTJGMkJGQUZGMzA2QjBCNkU3NTdBL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YmU5ZWMyYzMyZmVjNGJkODkwYzRmN2MyNTY4NzZiNTUma2V5Mj1iYzZmZGRmZDE0YWIwYTIwMmMzMWM1NjI0MmE1ZmJkOQ==";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8wNy9OYXBvbGlfdnNfTGl2ZXJwb29sXzIwMjIwOTA3XzE2NjI1NzA2MDEwMTgvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvMEVDRDBFNUI0QzA1NUQxQUZFQTZGQzdERjU2NzU4MjhDMkQ5RjA0MjI4M0RCRkE2QTAyMjhFRDk4REVCNkZDOC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PWIzN2U1OGVmMzY4YjQwYWM5Y2E5MTdlZjI0M2I4ZDNhJmtleTI9OTU2OTBhNDY0OGJiMmQ4MzFmMzNhNjVjNDNiYTllODE=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
