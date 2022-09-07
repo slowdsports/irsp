3a9fd2b3057056e0516a7872d55f8e85c200043c
Eduardo Perdomo <33327508+eduveel19@users.noreply.github.com>
2022-09-07 12:16:32 -0600

    Update channels.php

--- v3/agenda/futbol/liga/channels.php
+++ v3/agenda/futbol/liga/channels.php
@@ -32,6 +32,23 @@ elseif($canal2 === "M+ Liga de Campeones 2" || $canal2 === "mlcamp2"){
         Movistar Liga de Campeones | HD
         </a>
     </li>';
+   
+
+}  elseif($canal2 === "M+ Liga de Campeones 4" || $canal2 === "mlcamp4"){
+    $canalImg = "campeones";
+    $canalop4 = '
+    <li>
+        <a class="justify-content-center" href="../../play?c=mlcamp4">
+        <i class="flag es"></i>
+        Movistar Liga de Campeones 4 | VIP HD
+        </a>
+    </li>
+    <li>
+        <a class="justify-content-center" href="">
+        <i class="flag es"></i>
+        Movistar Liga de Campeones | HD
+        </a>
+    </li>';
 } elseif($canal2 === "M+ Liga de Campeones" || $canal2 === "M+ Liga de Campeones UHD (M441 O115)" || $canal2 === "M+ Liga de Campeones (M56 O114)" || $canal2 === "mlcamp"){
     $canalImg = "campeones";
     $canalop4 = '
