42be1c37b8f605149ecac49f08aca5a3ca8e8192
Alex <slowdsports@gmail.com>
2022-09-10 12:36:17 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -23,3 +23,16 @@ if ($result['visita'] === "torino"){
     </li>
     ';
 }
+
+if ($result['visita'] === "milan"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8xMC9TYW1wZG9yaWFfdnNfTWlsYW5fMjAyMjA5MTBfMTY2MjgyOTIxNDAyMS9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci81QTcxQUYwNERFMDdGMUU0RUQ0M0JFQUJGM0E0QTBERUQwNEQ0NkYxNTg4NDQ4NDNEODYwNzU1MkU5MTg1RTVDL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9NjE4MzM5NjRiNjNjNDI2ZTg1ZDE2ODU1MDIwODdmOTAma2V5Mj00ZGY1ZTVlNjkwMDY5YTA3MjIzNTM4OTczMWE4YTdiMA==";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
