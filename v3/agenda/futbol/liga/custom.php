fac3e29c8fab86d455616c3101058cb1e1c957eb
Eduardo <eperdomo833@gmail.com>
2022-09-12 23:36:16 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -11,9 +11,87 @@ if ($result['visita'] === "team"){
     </li>
     ';
 }
-if ($result['local'] === "juventus"){
+if ($result['local'] === "plzen"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8xMS9KdXZlbnR1c192c19TYWxlcm5pdGFuYV8yMDIyMDkxMV8xNjYyOTE1NjEwMDcyL2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyL0VEMDgyQjBCMjFGNDIwNjY2NTZFOUVGMjEyRjkzRDZCQzAwQzdDMzdGMkQyMzk5NTJEMDk1MzRERkRCQ0I1Mjkvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT03OGE3Mjc4MjgyNTg0ODA3ODkwY2E0OGE0ZDc4MmFjYiZrZXkyPWU5M2YzMDdiMjFjMDE5NmVjZjZkZTkyZWU3YjcyZGUy";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['visita'] === "tottenham"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "bayern"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "leverkusen"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "liverpool"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "marseille"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "porto"){
+    $canalImg = "starplus";
+    $r= "";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
