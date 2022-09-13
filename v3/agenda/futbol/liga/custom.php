74fac3bde326650322b95e8c7c4435d5c107eda8
Eduardo <eperdomo833@gmail.com>
2022-09-13 11:00:56 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -13,7 +13,7 @@ if ($result['visita'] === "team"){
 }
 if ($result['local'] === "plzen"){
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8xMy9GQ19WaWt0b3JpYV9QbHplbl92c19JbnRlXzIwMjIwOTEzXzE2NjMwODEyMTUwNTkvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvOTNEQkE2REJBNkRBNEEyNTg0RDEyRDUzQkVBOTE3QzQ1Q0NDM0Y1NTg4MUJEOEQzMzAwQTFGRjBGRjZBMDUwQi9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTYyODNlNDgwOTc0YjRkMjFiMDhjZTJkNzUxMDFjMWZmJmtleTI9MTE4NTZiMDhjOWI3ZmMyZmU5ZmVjMGFkZWRiNGM3MDE=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -26,7 +26,7 @@ if ($result['local'] === "plzen"){
 
 if ($result['visita'] === "tottenham"){
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8xMy9TQ19TcG9ydGluZ19Qb3J0dWdhbF92c19UXzIwMjIwOTEzXzE2NjMwODEyMzUwMzUvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQ0I4MUE0QjUyQUUwNjMwRURCQjZFRDg3MTE0RjNGQTUxNUE0QzhFMTU1MDNENUJFMUU4RkJDREVBOEIzOUIwNS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTMyNmY4OTQ0Y2UyNTQxYThiZmUyZDhhNWQ1NzNlN2QwJmtleTI9YmRjZWU2MmY2NWIwYTY2NzVkNDNhNTQwYjE3NzJjMWM=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
