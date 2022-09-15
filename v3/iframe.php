dd4beda8121c59e69ee8e7ada3206081c72cc9bb
Alex <slowdsports@gmail.com>
2022-09-15 00:34:34 -0600

    Update iframe.php

--- v3/iframe.php
+++ v3/iframe.php
@@ -1,1 +1,10 @@
-<iframe src="https://gregorsamsa.life/embed/es/vamos.php" allow="autoplay; encrypted-media" allowfullscreen sandbox="allow-scripts allow-same-origin" frameborder="0"></iframe>
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'dfc0d035bbd97c70f01215bd14145593',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/dfc0d035bbd97c70f01215bd14145593/invoke.js"></scr' + 'ipt>');
+</script>
