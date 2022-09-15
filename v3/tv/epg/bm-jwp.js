eb4166d1bc8b9f761196a5b34f0a3b226f963bfe
Alex <slowdsports@gmail.com>
2022-09-15 02:15:13 -0600

    Update bm-jwp.js

--- v3/tv/epg/bm-jwp.js
+++ v3/tv/epg/bm-jwp.js
@@ -8,9 +8,9 @@ playerInstance.setup({
                 "widevine": {
                     "headers": [{
                         "name": "authorization",
-                        "value": atob("T0F1dGggb2F1dGhfY29uc3VtZXJfa2V5PTQ0ODdmYjY1NGRmZjUyMWYyNWUwMzhkMTNmY2U2N2ViY2MwNmI0ZWJlNjY4YWQ4ZmUyNWI3ODZlYzNjY2FhOGIsIG9hdXRoX25vbmNlPW01R1RMRywgb2F1dGhfc2lnbmF0dXJlPWN4RmRiV2hVdk1UQlJiWUpzY0dZek5aRmNSZyUzRCwgb2F1dGhfc2lnbmF0dXJlX21ldGhvZD1ITUFDLVNIQTEsIG9hdXRoX3RpbWVzdGFtcD0xNjUwNTg0OTE4LCBvYXV0aF92ZXJzaW9uPTEuMA==")
+                        "value": atob(getKEY)
                     }],
-                    "url": atob("Ly9hcGkuYmxpbS5jb20vbGljZW5zZS93aWRldmluZQ==")
+                    "url": atob(getKEY2)
                 }
             }
         }]
