a7da370b6107d5a23bef3a1105077fe9169ecef5
Alex <slowdsports@gmail.com>
2022-09-21 13:38:11 -0600

    Update valid.php

--- v3/tv/epg/valid.php
+++ v3/tv/epg/valid.php
@@ -116,7 +116,8 @@ elseif ($typeChannel == 4){
     $pKey=mysqli_query($conn,"select * from photocall");
     $ret=mysqli_fetch_assoc($pKey);
     $pKey = $ret['pKey'];
-    $pcSrc = "http://irsitpv.42web.io/pc/list.php?id=" . $ret['pKey'] . "/" . $result['channelUrl'];
+    //$pcSrc = "http://irsitpv.42web.io/pc/list.php?id=" . $ret['pKey'] . "/" . $result['channelUrl'];
+    $pcSrc = "https://irpc.ga/pc/list-test.php?id=" . $ret['pKey'] . "/" . $result['channelUrl'];
     echo '<style>#iframe-element{display:none;} #myTab{display:none} #jwp{display:none} #vidarea{display:none} #player_img img{display:block!important}</style>';
 } // Blim
 if ($typeChannel == 6){
