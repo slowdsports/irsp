ca14f863f5d7c67496185d8412c7a29a815cefac
Alex <slowdsports@gmail.com>
2022-09-20 23:38:13 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -13,6 +13,147 @@ if ($result['visita'] === "team"){
     ';
 }
 
+if ($result['local'] === "escocia"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['local'] === "kazajistn"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+if ($result['local'] === "letonia"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "francia"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "blgica"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "polonia"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "croacia"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "turqua"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "lituania"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "eslovaquia"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "liechtensein"){
+    $canalImg = "starplus";
+    $r= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
 
 //HBO
 if ($result['visita'] === "team"){
@@ -23,30 +164,6 @@ if ($result['visita'] === "team"){
         <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
         <i class="flag ar"></i>
         HBO Max | HD
-        </a>
-    </li>
-    ';
-}
-if ($result['visita'] === "arsenal"){
-    $canalImg = "hbow";
-    $s= "aHR0cHM6Ly9saXZlLmNmLmxhdGFtLmhib21heGNkbi5jb20vY21hZi9saXZlLzIwMzE2ODIvZGFzaC9jZW5jLzI3ODUyL21hc3Rlci5tcGQ=&key=MDEwMTQ5YmYxYjhkNTUyNzU2OWExOGVlNmJhNjExMjc=&key2=Y2RjOWZkYzRmOGI2MmVkMWFiNDUzNmE3MGM2OTNkN2Y=";
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
-        <i class="flag ar"></i>
-        HBO Max | HD
-        </a>
-    </li>
-    ';
-}
-if ($result['visita'] === "baltimore"){
-    $canalImg = "nfl";
-    $s= "";
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="http://d2wd103krdtde9.cloudfront.net/hls/ravens/playlist.m3u8">
-        <i class="flag us"></i>
-        Ravens
         </a>
     </li>
     ';
