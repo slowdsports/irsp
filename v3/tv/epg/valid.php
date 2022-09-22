b4c979c7dd189f9097f83120af6070ff50ebc2c1
Alex <slowdsports@gmail.com>
2022-09-21 22:42:36 -0600

    Update valid.php

--- v3/tv/epg/valid.php
+++ v3/tv/epg/valid.php
@@ -118,7 +118,10 @@ elseif ($typeChannel == 4){
     $pKey = $ret['pKey'];
     //$pcSrc = "http://irsitpv.42web.io/pc/list.php?id=" . $ret['pKey'] . "/" . $result['channelUrl'];
     $pcSrc = "https://irpc.ga/epg/?id=" . $ret['pKey'] . "/" . $result['channelUrl'];
-    echo '<style>#iframe-element{display:none;} #myTab{display:none} #jwp{display:none} #vidarea{display:none} #player_img img{display:block!important}</style>';
+    echo '<style>#iframe-element{display:none;} #myTab{display:none} #jwp{display:none} #vidarea{display:none} #player_img img{display:none!important}</style>';
+    echo '<script>
+    location.href = '.$pcSrc.';
+    </script>';
 } // Blim
 if ($typeChannel == 6){
     // Recargar página con SSL
