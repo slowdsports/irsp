8b4eeffded3a66e749aed7b1aaa8b0b04b2410a8
Alex <slowdsports@gmail.com>
2022-09-21 23:54:34 -0600

    Update index.php

--- v3/tv/index.php
+++ v3/tv/index.php
@@ -120,7 +120,7 @@ if (isset($_SESSION['message']) ){
                         <div class="card product-card liga-card">
                             <div class="card-body">
                                 <center>
-                                <img width="48px" src="https://i.ibb.co/w0qg9JF/trans.png" style="background-image: url('<?=$app?>/assets/img/canales/<?=$result['channelImg']?>');
+                                <img width="48px" src="https://i.ibb.co/w0qg9JF/trans.png" style="background-image: url('<?=$app?>/assets/img/canales/<?=$result['channelImg']?>.png');
                                 background-size: contain;
                                 background-repeat: no-repeat" class="image" alt="product image">
                                 <h2 class="title text-center"><?=strtoupper($result['channelName'])?></h2>
