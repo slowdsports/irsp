a27b50fbbe675d13db22a3736270b871c7302025
Alex <slowdsports@gmail.com>
2022-09-23 12:05:43 -0600

    asdasd

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -39,7 +39,7 @@ if ($result['local'] === "alemania"){
 }
 if ($result['local'] === "brasil"){
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yMy9CcmF6aWxfdnNfR2hhbmFfMjAyMjA5MjNfMTY2Mzk1MTgyNDAyMi9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci85MUUxMTE2MEM0NTI2QjA0MjVBNDA0MjYzMjBCMDI1RDE5NDE4ODBCNUY2MzZENkEwODQyOEJBRkY5QzZFODkwL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9ZmFlNTRhMWRmZjA2NDBlZWI2MWRlNjUzMDhhNDFjMTIma2V5Mj1lYWMzN2MyNDY3OGY5NDdhYTk0ZmRkMTU4NGY5MDNjNg==";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
