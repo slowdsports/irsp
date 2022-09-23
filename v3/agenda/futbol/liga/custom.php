213da91ae084786b9df31b48962ee0014caec734
Alex <slowdsports@gmail.com>
2022-09-23 12:39:12 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -15,7 +15,7 @@ if ($result['visita'] === "team"){
 
 if ($result['local'] === "italia"){
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yMy9JdGFseV92c19JbmdsYXRlcnJhXzIwMjIwOTIzXzE2NjM5NTI0MDA0NDAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvODJCRjg5RDI1NUNCOTVFQTBGQThDQkVFMUFBNkFBRDc0Nzk1MUNEMDg3RjdBRDMxN0VEQTExRjg2QTgzOEJEQS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PWRmNTQzN2EzMTVjNjRlNjRiNTE3NTU0OTMwMGY3ZGM3JmtleTI9NmQxNzdlNWJjZjAyZWEwYzFmNWU0OGQzNmFkNDUwOGI=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -27,7 +27,7 @@ if ($result['local'] === "italia"){
 }
 if ($result['local'] === "alemania"){
     $canalImg = "starplus";
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yMy9HZXJtYW55X3ZzX0h1bmdyYV8yMDIyMDkyM18xNjYzOTUyNDQ4MDIyL2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyL0I3NzFBRTI1RjhCNjUwMTk0RjQyRTVFODlBMUJEMzM2ODJDOUE2M0FDODc3MkJFNEM0MkU1MkI5NUVGQzg0NjEvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT1lNzE1ZTA1OWJlODM0ZmM4ODVkOGRhM2M0MGI2MjQzZCZrZXkyPWEwMmYzYTgwNjNkMzdlMzAxMzUyNzcwMDQ1ZjI4ZDZj";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
