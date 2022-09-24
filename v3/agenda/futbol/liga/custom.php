8edd4c6daee9ecf3b969e0abc4e28db5af933399
Alex <slowdsports@gmail.com>
2022-09-24 12:49:25 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -39,6 +39,58 @@ if ($result['visita'] === "suiza"){
     ';
 }
 
+if ($result['local'] === "israel"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNC9Jc3JhZWxfdnNfQWxiYW5pYV8yMDIyMDkyNF8xNjY0MDM5MTI0MDE2L2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzg3NkMyMTYyNzI5OEZFM0E4RjYyODI0MDI0OTVENTU2NjE0OURFREEwMDMxNDM2MjM5Mzc5OENBMjUxMERCQzAvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT1jYWI3MmRmMzBmMzQ0YTI5ODUyNzVjZGRhYzk0NDNkMyZrZXkyPTk5MzJmODhmZTMzNzllMDRjOGY0MzAxMzdjNTI4YzFl";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "serbia"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNC9TZXJiaWFfdnNfU3VlY2lhXzIwMjIwOTI0XzE2NjQwMzg4MTUwNDUvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvMDNGRDIxQkNEMkM3NkRDRTlBQTFBNjI1OUI4RTYyMDhCMUFERTUwOEZCNDVGMEJDM0Y1Q0IyMTRDRDdGRTk3Ri9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTNiYmNjMzQ3YmRiNDQ5YmE4ZGVkZTE0ZWVhNDYzMjhjJmtleTI9OWUzN2QxMTc3MzYwMjBlNTg2MWNjMzRiNTBhZjcyNTI=";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "escocia"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNC9Fc2NvY2lhX3ZzX0lybGFuZGFfMjAyMjA5MjRfMTY2NDAzODgzNzAxMy9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci80NjJCMkVGREJFQ0Q5Q0QxODAwQjVEMDAzQjg2RjI0RkNCNkM5RTUxMTg3RkJENkNEMUY4OTcyNjlCQkJEMjYyL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YmMxYWQ2OWU2NjFkNDE2M2ExMmIzYmFmY2FlMTNkODQma2V5Mj1kYWFiYzBlMTFhYzM4NGRmYjA1MGRkMGI5NDA5ODNjNQ==";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "chipre"){
+    $canalImg = "starplus";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNC9DaGlwcmVfdnNfR3JlY2lhXzIwMjIwOTI0XzE2NjQwMzkxNTUwMjYvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvMkEzNTE4RTQxM0E4REVGRkI4RkU3QzhEMkI2RkU3QkM4RTQwRjRDNjk1QzFGMTczQzBENDJEQjRFQUExOTQ1OC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PWFjYTFkOWZhMWQyODRmZDZiNTBmZGQxM2YzNzNmMjE3JmtleTI9M2Y3MjVmMTM3MmU5ODU4OWIxOTk1NmM5ODhhMjIzN2Y=";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
 
 
 
