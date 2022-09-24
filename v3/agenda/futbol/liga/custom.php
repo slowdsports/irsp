fa426fcbf65cc3c6dfb5e2efd8b32cb4f437cefc
Alex <slowdsports@gmail.com>
2022-09-24 12:47:05 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -13,9 +13,9 @@ if ($result['visita'] === "team"){
     ';
 }
 
-if ($result['local'] === "italia"){
+if ($result['visita'] === "portugal"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yMy9JdGFseV92c19JbmdsYXRlcnJhXzIwMjIwOTIzXzE2NjM5NTI0MDA0NDAvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvODJCRjg5RDI1NUNCOTVFQTBGQThDQkVFMUFBNkFBRDc0Nzk1MUNEMDg3RjdBRDMxN0VEQTExRjg2QTgzOEJEQS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PWRmNTQzN2EzMTVjNjRlNjRiNTE3NTU0OTMwMGY3ZGM3JmtleTI9NmQxNzdlNWJjZjAyZWEwYzFmNWU0OGQzNmFkNDUwOGI=";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNC9DemVjaF9SZXB1YmxpY192c19Qb3J0dWdhXzIwMjIwOTI0XzE2NjQwMzkxMDgwMjIvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvQzM5MEQ1QjlDOTc2NEQwMzdDNjgxMTk2NkVENTZEMkNEQzY4NzIzQkNGQ0NEQTNFMkVBQjExNTk2RTgyN0Y2Ri9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTUxYTkwN2I4ZGNjMjQyYTI5OWU3ZTg0YTI2ODZkNjZmJmtleTI9NGZkYTUyOGFjYjMwNDJmOTY5MThkMGNkMmFjOWI5NDQ=";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -25,9 +25,10 @@ if ($result['local'] === "italia"){
     </li>
     ';
 }
-if ($result['local'] === "alemania"){
+
+if ($result['visita'] === "suiza"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yMy9HZXJtYW55X3ZzX0h1bmdyYV8yMDIyMDkyM18xNjYzOTUyNDQ4MDIyL2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyL0I3NzFBRTI1RjhCNjUwMTk0RjQyRTVFODlBMUJEMzM2ODJDOUE2M0FDODc3MkJFNEM0MkU1MkI5NUVGQzg0NjEvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT1lNzE1ZTA1OWJlODM0ZmM4ODVkOGRhM2M0MGI2MjQzZCZrZXkyPWEwMmYzYTgwNjNkMzdlMzAxMzUyNzcwMDQ1ZjI4ZDZj";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNC9Fc3BhYV92c19TdWl6YV8yMDIyMDkyNF8xNjY0MDM4ODQ2MDQ4L2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzVGNkE2MkY4RkI4NzQ2MDg2NDlGQjFEMjQ5RDY2MzQxMDY3NjZFQTEwRjZGNTNGOEJEQzE3RERDMjMxNDNDODUvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT05YmIxZjljYmMxNjA0MjgyOTllODRkZGViMTY2NWRmNSZrZXkyPTExNmM1ZmQ3MGNkYjQ5YzM5Mjk5NmZkYzUzYTZjZThj";
     $custom1 = '
     <li>
         <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
@@ -37,18 +38,8 @@ if ($result['local'] === "alemania"){
     </li>
     ';
 }
-if ($result['local'] === "brasil"){
-    $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yMy9CcmF6aWxfdnNfR2hhbmFfMjAyMjA5MjNfMTY2Mzk1MTgyNDAyMi9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci85MUUxMTE2MEM0NTI2QjA0MjVBNDA0MjYzMjBCMDI1RDE5NDE4ODBCNUY2MzZENkEwODQyOEJBRkY5QzZFODkwL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9ZmFlNTRhMWRmZjA2NDBlZWI2MWRlNjUzMDhhNDFjMTIma2V5Mj1lYWMzN2MyNDY3OGY5NDdhYTk0ZmRkMTU4NGY5MDNjNg==";
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
+
+
 
 
 //HBO
