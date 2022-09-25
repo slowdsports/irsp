d38e0d46602284e38472216a8b3ac4bb5adb2713
Alex <slowdsports@gmail.com>
2022-09-24 22:54:56 -0600

    Update mg.php

--- v3/mg.php
+++ v3/mg.php
@@ -1,5 +1,11 @@
 <?php
 include('con.php');
-$userIP = $_SERVER['REMOTE_ADDR'];
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $userIP = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $userIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $userIP = $_SERVER['REMOTE_ADDR'];
+}
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 echo "IP: " . $userIP . "<br> UA: " . $userAgent;
