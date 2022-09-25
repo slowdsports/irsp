3856135cff8f012b9cf42e730b2a993401e7b6f0
Alex <slowdsports@gmail.com>
2022-09-24 22:48:55 -0600

    Update mg.php

--- v3/mg.php
+++ v3/mg.php
@@ -1,43 +1,4 @@
-<script src="https://ssl.p.jwpcdn.com/player/v/8.24.0/jwplayer.js"></script>
-<script> jwplayer.key='XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';</script>
-
 <?php
-$getURL = $_GET['get'];
-$getKEY = $_GET['key'];
-$getKEY2 = $_GET['key2'];
-echo '
-<script>
-let getURL = "'.$getURL.'";
-let getKEY = "'.$getKEY.'";
-let getKEY2 = "'.$getKEY2.'";
-</script>';
-?>
-
-<div id="player"></div>
-<script>
-var mt = "//edge-live15-sl";
-var playerInstance = jwplayer("player");
-playerInstance.setup({
-    playlist: [
-        {
-            sources: [
-                {
-                    default: false,
-                    file: mt + atob(getURL),
-                    drm: {
-                        clearkey: { keyId: atob(getKEY), key: atob(getKEY2) },
-                    },
-                    label: "0",
-                },
-            ],
-        },
-    ],
-    width: "100%",
-    height: "100%",
-    aspectratio: "16:9",
-    mute: false,
-    autostart: false,
-    cast: {},
-    sharing: {},
-});
-</script>
+include('con.php');
+$userIP = $_SERVER['REMOTE_ADDR'];
+echo $userIP;
