05843531fd73973cdc05a67efd4937b02ecb0d3d
Alex <slowdsports@gmail.com>
2022-09-25 17:50:15 -0600

    Update stream.php

--- v3/agenda/nfl/stream.php
+++ v3/agenda/nfl/stream.php
@@ -86,3 +86,23 @@ if(isset($_GET['g'])){
         echo '<script>window.location.href = "../../tv/epg?nba&plyr=jw&c='.$m3u8.'&id='.$index.'";</script>';
     }
 }
+
+if($canal === "M+ #Vamos" || $canal === "M+ #Vamos Bar 2 (305)" || $canal === "mvamos"){
+    $canalImg = "mvamos";
+    $canalop2 = '
+    <li>
+        <a class="justify-content-center" href="'.$app.'/agenda/play?c=mvamos">
+        <i class="flag es"></i>
+        Movistar #Vamos | VIP HD
+        </a>
+    </li>';
+} elseif($canal2 === "M+ #Vamos" || $canal2 === "M+ #Vamos Bar 2 (305)" || $canal2 === "mvamos"){
+    $canalImg = "mvamos";
+    $canal2 = '
+    <li>
+        <a class="justify-content-center" href="'.$app.'/agenda/play?c=mvamos">
+        <i class="flag es"></i>
+        Movistar #Vamos | VIP HD
+        </a>
+    </li>';
+}
