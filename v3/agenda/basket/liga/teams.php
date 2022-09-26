b40e19e187899776f389ff0f21054e32986ad372
Alex <slowdsports@gmail.com>
2022-09-25 19:43:37 -0600

    assdc

---
+++ v3/agenda/basket/liga/teams.php
@@ -0,0 +1,40 @@
+<?php
+// Equipos
+    // Endesa
+    $local = str_replace('bsquetgirona', 'Bàsquet Girona', $local);
+    $local = str_replace('carplusfuenlabrada', 'Carplus Fuenlabrada', $local);
+    $local = str_replace('monbusobradoiro', 'Monbus Obradoiro', $local);
+    $local = str_replace('baximanresa', 'BAXI Manresa', $local);
+    $local = str_replace('ucammurcia', 'UCAM Murcia', $local);
+    $local = str_replace('cazoobaskonia', 'Cazoo Baskonia', $local);
+    $local = str_replace('coosurrealbetis', 'Coosur Real Betis', $local);
+    $local = str_replace('grancanaria', 'Gran Canaria', $local);
+    $local = str_replace('joventut', 'Joventut', $local);
+    $local = str_replace('realmadrid', 'Real Madrid', $local);
+    $local = str_replace('covirngranada', 'Covirán Granada', $local);
+    $local = str_replace('casademontzaragoza', 'Casademont Zaragoza', $local);
+    $local = str_replace('lenovotenerife', 'Lenovo Tenerife', $local);
+    $local = str_replace('robreogn', 'Río Breogán', $local);
+    $local = str_replace('unicajamlaga', 'Unicaja Málaga', $local);
+    $local = str_replace('valenciabasket', 'Valencia Basket', $local);
+    $local = str_replace('bara', 'Barça', $local);
+    $local = str_replace('surnebilbaobasket', 'Surne Bilbao Basket', $local);
+    
+    $visita = str_replace('bsquetgirona', 'Bàsquet Girona', $visita);
+    $visita = str_replace('carplusfuenlabrada', 'Carplus Fuenlabrada', $visita);
+    $visita = str_replace('monbusobradoiro', 'Monbus Obradoiro', $visita);
+    $visita = str_replace('baximanresa', 'BAXI Manresa', $visita);
+    $visita = str_replace('ucammurcia', 'UCAM Murcia', $visita);
+    $visita = str_replace('cazoobaskonia', 'Cazoo Baskonia', $visita);
+    $visita = str_replace('coosurrealbetis', 'Coosur Real Betis', $visita);
+    $visita = str_replace('grancanaria', 'Gran Canaria', $visita);
+    $visita = str_replace('joventut', 'Joventut', $visita);
+    $visita = str_replace('realmadrid', 'Real Madrid', $visita);
+    $visita = str_replace('covirngranada', 'Covirán Granada', $visita);
+    $visita = str_replace('casademontzaragoza', 'Casademont Zaragoza', $visita);
+    $visita = str_replace('lenovotenerife', 'Lenovo Tenerife', $visita);
+    $visita = str_replace('robreogn', 'Río Breogán', $visita);
+    $visita = str_replace('unicajamlaga', 'Unicaja Málaga', $visita);
+    $visita = str_replace('valenciabasket', 'Valencia Basket', $visita);
+    $visita = str_replace('bara', 'Barça', $visita);
+    $visita = str_replace('surnebilbaobasket', 'Surne Bilbao Basket', $visita);
