a82a651d3288f09ad0ecc9f543e3df3d893a12d2
Alex <slowdsports@gmail.com>
2022-09-27 12:42:26 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -125,7 +125,7 @@ if ($result['local'] === "grecia"){
 if ($result['local'] === "suiza"){
     $canalImg = "starplus";
     // LINK:
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9TdWl6YV92c19DemVjaF9SZXB1YmxpY18yMDIyMDkyN18xNjY0Mjk4MDQ4MDIyL2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzg0RDA5NEM2NTBGMkNEOTA5NzUxMzI0RDlBQTg0NEUwOEMwRTU3NzU2NkU5RDU5RkI0RjRCRDBERkFEREVFRkYvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT0zNjc1NDQ5MjZmYWE0YzRjOWNlNzFhMDk4ODNmOGJkNCZrZXkyPWQzMWYxZGQ1YzNlNWRiZTlhYTI3MDk0OTM5ZGE3MWQ0";
 
     if ($r !== ""){
         $r = "../../../tv/epg?r=" . $r;
@@ -142,12 +142,12 @@ if ($result['local'] === "suiza"){
     ';
 }
 
-if ($result['local'] === "armenia"){
+if ($result['visita'] === "armenia"){
     $canalImg = "starplus";
     // LINK:
     $r= "";
 
-    if ($r !== ""){
+    if ($r !== "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9JcmxhbmRhX3ZzX0FybWVuaWFfMjAyMjA5MjdfMTY2NDI5ODMxOTAyMi9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci84MzVFOUMxMEZGM0EzMEJFMDVBOTZFNjdCMUNBMjk0N0Y2QTc0NTY1OTg5RTQ5QUY2RTA1QjE3Q0Y1MjkwQkZCL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9M2JiMjg4YTYzNTg3NGMxMDhlNjQ3ZWU2ODY1NWY1ZjEma2V5Mj05ODA2MjVmYjM0MDc0ZWJlMWJkZjI2MWU2YmM1ZTE5YQ=="){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -165,7 +165,7 @@ if ($result['local'] === "armenia"){
 if ($result['local'] === "albania"){
     $canalImg = "starplus";
     // LINK:
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9BbGJhbmlhX3ZzX0lzbGFuZGlhXzIwMjIwOTI3XzE2NjQyOTgzMzgwMTgvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvOEI4NjI4MzlCMEQ0RUZBMEQ4Mzc0Q0ZBN0QxMzg1OTI1MUNEMTA3MDdCRUJEQkNBQzM3MTA4QTJGQzc4NTA3RC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTUxYzk3YTcxMmRlNDRjMzY5YzkzYjY0MTkwM2E5ZWZiJmtleTI9N2M4MTk4ZWY2NmVmNTg4MjgwM2IwYWYyZmVlZGM1MmY=";
 
     if ($r !== ""){
         $r = "../../../tv/epg?r=" . $r;
@@ -185,7 +185,7 @@ if ($result['local'] === "albania"){
 if ($result['local'] === "kosovo"){
     $canalImg = "starplus";
     // LINK:
-    $r= "";
+    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9Lb3Nvdm9fdnNfQ2hpcHJlXzIwMjIwOTI3XzE2NjQyOTgzNTEwMjEvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvRjE1ODI3MkFDQ0UwNkYyQTY4QURFMEE0MTYxRjE4OTAzMTUyRkEyMjlGRTlBQkU5OUJGNzRCRjczOTc3OUNERS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTM3OTY3NzE5NDU1MDRmZDI4OGU5ZmNiZmY2NTY4NmNlJmtleTI9NzU2ZTM2ODk3YjFkZmY5YmQxYWYzZGUxNTEyMzYxZGU=";
 
     if ($r !== ""){
         $r = "../../../tv/epg?r=" . $r;
