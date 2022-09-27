d360b3df7d5e5b39a1fe89dfeb2df581b8e7dc74
Alex <slowdsports@gmail.com>
2022-09-26 21:55:49 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -2,10 +2,17 @@
 // Star
 if ($result['visita'] === "team"){
     $canalImg = "starplus";
+    // LINK:
     $r= "";
+
+    if ($r === ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <a class="justify-content-center" href="'.$r.'">
         <i class="flag ar"></i>
         Star + | HD
         </a>
@@ -13,36 +20,39 @@ if ($result['visita'] === "team"){
     ';
 }
 
-if ($result['local'] === "holanda"){
+if ($result['local'] === "suecia"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNS9OZXRoZXJsYW5kc192c19CZWxnaXVtXzIwMjIwOTI1XzE2NjQxMjUyMjgwMjIvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvRDNCMzIwOEVGMjhCNTVBOTBERUZCQkRDMjBBNEZDNDAwOTEwNTI0Mjg3QzRBRUQ4MTlBNTlCQkUyNDY2RkE2MS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTNmZDllOTQzNGE5YjRhMWViNTJmYjJhYjcyMzBmMjg3JmtleTI9NjEzYTg2Y2U0NjBjNjQwNWJiNjBiNTIzYWZmODI0M2M=";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <a class="justify-content-center" href="'.$r.'">
         <i class="flag ar"></i>
         Star + | HD
         </a>
     </li>
     ';
 }
-if ($result['local'] === "dinamarca"){
+
+if ($result['local'] === "portugal"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNS9EaW5hbWFyY2FfdnNfRnJhbmNpYV8yMDIyMDkyNV8xNjY0MTI1MjE5MDI1L2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzBENEVFODA1MDg1RTExNUE5RUUwRUYyMjBBNDU2QzU2ODY5NEY3N0I5M0UzMTQ3REExMDVEMDg0QTExQjI0QUYvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT1iZjk4M2YxM2NhZDg0NDdjYmU5NjJlYzRkNTQ1YWMzZCZrZXkyPTcwMWFjMTcyYTMxN2FlNTU0ZWMyYTkwM2YwYWQ2YmM4";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-if ($result['local'] === "austria"){
-    $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNS9BdXN0cmlhX3ZzX0Nyb2F0aWFfMjAyMjA5MjVfMTY2NDEyNTIwMTAxNC9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci8yQ0Y3OThEMDRDRENGRDgxNTk2ODYwNjI4RkZCNjc4QjgzMTUwODEwNzQ4MkVBMDBGOTA1RUFDRDg1N0Q5OUExL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9NzhiOWQ2M2E4YzdjNGU2NTllNzVlYmQ1MjZlYWEyYTAma2V5Mj04NWU0MDlmZDYwZTMzOTljNmFhZWZmMTkzODljNzM1Yw==";
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <a class="justify-content-center" href="'.$r.'">
         <i class="flag ar"></i>
         Star + | HD
         </a>
@@ -51,12 +61,19 @@ if ($result['local'] === "austria"){
 }
 
 
-if ($result['local'] === "islasferoe"){
+if ($result['local'] === "noruega"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNS9Jc2xhc19GYXJvZV92c19UdXJrZXlfMjAyMjA5MjVfMTY2NDEyNTUzMjAyMy9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci8wRTgyQjUyMzJBOTVCRTk3REZDMjZEQUQyNjNFOTQ2Q0ExMUQyQ0Q2Q0MxMUUzMkE1MjY1NTE4OEFDODM3M0Y2L3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9ZGRlMWYyZjc5MzM0NDkwN2IyYjQ5YTEzODZhNmQzN2Mma2V5Mj04N2YwMWM5MDUwNTQ1YWRmMTg4YzcyMDlkZjQ1YzcxYw==";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <a class="justify-content-center" href="'.$r.'">
         <i class="flag ar"></i>
         Star + | HD
         </a>
@@ -64,12 +81,19 @@ if ($result['local'] === "islasferoe"){
     ';
 }
 
-if ($result['local'] === "luxemburgo"){
+if ($result['local'] === "ucrania"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9jbHQyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNS9MdXhlbWJ1cmdvX3ZzX0xpdHVhbmlhXzIwMjIwOTI1XzE2NjQxMjU1MDIwMjgvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvMUI3OENGQ0U4OTM5MkVGNTg5NTc1MzQwMTI3NkU0OEUxRjJENjFBOEUzNjJCNDM0NkE5Mjk3QUEwRTM3N0ZCRi9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PWI0ZTNlYjQzZDRmNjRiZDY5Y2E1ZTU2MzRmYTQyNzg0JmtleTI9OGIwMzBiOWQ2MDUyYTRkNTgxMjM2NWI3ZGI5M2Y3OWE=";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <a class="justify-content-center" href="'.$r.'">
         <i class="flag ar"></i>
         Star + | HD
         </a>
@@ -77,12 +101,100 @@ if ($result['local'] === "luxemburgo"){
     ';
 }
 
-if ($result['local'] === "gales"){
+
+if ($result['local'] === "grecia"){
     $canalImg = "starplus";
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNS9HYWxlc192c19Qb2xhbmRfMjAyMjA5MjVfMTY2NDEyNTUyMzAxNi9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci9FQ0ZBMDVDRENBNkQ1QUNCN0JERDQxMTBEQ0EyRjA4QTUxMEI1N0RDMjhFNUEwQ0Y5OTQ3Rjk0OTRDMUNGNkRCL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9NjRhMmUxM2FlZjEwNGU4YjhhODVhYTEyNTJlNDNkMjYma2V5Mj1jNTA1MmMyOTgwMWM5NTBkYzFmNDE0MDA5NjcxOTc0Nw==";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
     $custom1 = '
     <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$r.'">
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "suiza"){
+    $canalImg = "starplus";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "armenia"){
+    $canalImg = "starplus";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "albania"){
+    $canalImg = "starplus";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "kosovo"){
+    $canalImg = "starplus";
+    // LINK:
+    $r= "";
+
+    if ($r !== ""){
+        $r = "../../../tv/epg?r=" . $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
         <i class="flag ar"></i>
         Star + | HD
         </a>
