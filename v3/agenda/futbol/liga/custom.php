8716b52482d7f43bab3e363637e701d89ef3f283
Alex <slowdsports@gmail.com>
2022-10-01 01:16:11 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -1,6 +1,6 @@
 <?php
 // Star
-if ($result['visita'] === "team"){
+if ($result['local'] === "team"){
     $canalImg = "starplus";
     // LINK:
     $r= "";
@@ -20,12 +20,12 @@ if ($result['visita'] === "team"){
     ';
 }
 
-if ($result['local'] === "suecia"){
+if ($result['visita'] === "chelsea"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9TdWVjaWFfdnNfU2xvdmVuaWFfMjAyMjA5MjdfMTY2NDI5ODA1MzAzOS9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci8xQzY2N0JGQjUyQzAyMDQ1NjNCQzNERkE0NDAwQzFERjlDODlDNDcyNTM1RUYxNzRGRjgxNTRCMDYwRTVEMzUyL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9M2U3ZGUzNjJlZWUzNGUxNDllYWVhOWM2ODI3M2Q5MTUma2V5Mj1jOGE4NDllZGM0MDlhODMyNTkxMDMwMWIyM2RkOGZmNjc=";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -40,12 +40,12 @@ if ($result['local'] === "suecia"){
     ';
 }
 
-if ($result['local'] === "portugal"){
+if ($result['visita'] === "everton"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9Qb3J0dWdhbF92c19Fc3BhYV8yMDIyMDkyN18xNjY0Mjk4MDQ3MDE0L2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzgwNkU5NDUzQjg4MzIxODAxQ0ZFNjdDNjA0MjE3MzREMEExRjAwRTE5M0U5ODk5NDhDNUE4NjI5ODJEMUQyNkMvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT1hN2QwYTBkM2JmNGQ0MWRkOTE2ZDY3ZjY2MzYwMmIyOSZrZXkyPTQ1OTA4ZWViZTBjNjdjZTNjZjgwNWYyMzg3MTdhOGIy";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -60,13 +60,12 @@ if ($result['local'] === "portugal"){
     ';
 }
 
-
-if ($result['local'] === "noruega"){
+if ($result['local'] === "fulham"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9Ob3J1ZWdhX3ZzX1NlcmJpYV8yMDIyMDkyN18xNjY0Mjk4MDQ0MDIxL2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyL0IzMjlEQjZFN0E0MzhBNEU1NUREQjI1NkIwM0Q4QTA3NURBMzMzQzIzNzU1RkY0RjQ0MzYwODE2OUE5ODg4QTIvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT05MTQ2NmM3MTgwOTc0ZDIxYWMwZjE3OWE4NWQ3NWVkNiZrZXkyPWRiMjA4N2FlYzE2OTVjMDg2M2NjNmU4ZTY2ZmVjMTMzNWY4Nw==";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -81,12 +80,12 @@ if ($result['local'] === "noruega"){
     ';
 }
 
-if ($result['local'] === "ucrania"){
+if ($result['visita'] === "brentford"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9Va3JhaW5lX3ZzX0VzY29jaWFfMjAyMjA5MjdfMTY2NDI5NzQzNjAyMy9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci8xOTE4NzI5NjcwQkM3QThBQTg0QzdBMDRCRjBGQTExNDM0MTk1Nzk1MDBBMUE2QUVDNDEzNjJFMEYxM0RCMTcyL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9YTBiOTEwNjk5ZmYwNDlhYjliZDFiMjk0NjFkOGNkZmMma2V5Mj0xNjBiODhjZTNmYzM5NGU5NGFmYWE1ZmJlOTY3NWY4Nw==";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -101,13 +100,12 @@ if ($result['local'] === "ucrania"){
     ';
 }
 
-
-if ($result['local'] === "grecia"){
+if ($result['local'] === "inter"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9HcmVjaWFfdnNfSXJsYW5kYV9kZWxfTm9yXzIwMjIwOTI3XzE2NjQyOTgzMjMwMjQvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvN0Y5NDVBMkRDNjdCQjkwNjE0MkNBMTZDNkE1NDk1OTZDNEUyQzk0RTFCNzYxNzc0MUVBNENEOUVDODQwQ0IyQy9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTI5NzJmYTI2Yzc4OTQ1MTc4MmIwMDJjMzAyYWY5NGY0JmtleTI9ZjZjOTQ0ZDIzZDI5YmIxOGRiNmJjZjA4Y2NiYzM5NzA=";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -122,12 +120,12 @@ if ($result['local'] === "grecia"){
     ';
 }
 
-if ($result['local'] === "suiza"){
+if ($result['local'] === "sevilla"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9ncnUxL3FiMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9TdWl6YV92c19DemVjaF9SZXB1YmxpY18yMDIyMDkyN18xNjY0Mjk4MDQ4MDIyL2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyLzg0RDA5NEM2NTBGMkNEOTA5NzUxMzI0RDlBQTg0NEUwOEMwRTU3NzU2NkU5RDU5RkI0RjRCRDBERkFEREVFRkYvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT0zNjc1NDQ5MjZmYWE0YzRjOWNlNzFhMDk4ODNmOGJkNCZrZXkyPWQzMWYxZGQ1YzNlNWRiZTlhYTI3MDk0OTM5ZGE3MWQ0";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -142,12 +140,12 @@ if ($result['local'] === "suiza"){
     ';
 }
 
-if ($result['visita'] === "armenia"){
+if ($result['visita'] === "wolves"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9JcmxhbmRhX3ZzX0FybWVuaWFfMjAyMjA5MjdfMTY2NDI5ODMxOTAyMi9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci84MzVFOUMxMEZGM0EzMEJFMDVBOTZFNjdCMUNBMjk0N0Y2QTc0NTY1OTg5RTQ5QUY2RTA1QjE3Q0Y1MjkwQkZCL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9M2JiMjg4YTYzNTg3NGMxMDhlNjQ3ZWU2ODY1NWY1ZjEma2V5Mj05ODA2MjVmYjM0MDc0ZWJlMWJkZjI2MWU2YmM1ZTE5YQ==";
+    $r= "";
 
-    if ($r !== "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9JcmxhbmRhX3ZzX0FybWVuaWFfMjAyMjA5MjdfMTY2NDI5ODMxOTAyMi9jdHItYWxsLWNvbXBsZXRlLm0zdTgmaW1nPWh0dHBzOi8vcHJvZC1yaXBjdXQtZGVsaXZlcnkuZGlzbmV5LXBsdXMubmV0L3YxL3ZhcmlhbnQvc3Rhci84MzVFOUMxMEZGM0EzMEJFMDVBOTZFNjdCMUNBMjk0N0Y2QTc0NTY1OTg5RTQ5QUY2RTA1QjE3Q0Y1MjkwQkZCL3NjYWxlP3dpZHRoPTE5MjAmYXNwZWN0UmF0aW89MS43OCZmb3JtYXQ9anBlZyZrZXk9M2JiMjg4YTYzNTg3NGMxMDhlNjQ3ZWU2ODY1NWY1ZjEma2V5Mj05ODA2MjVmYjM0MDc0ZWJlMWJkZjI2MWU2YmM1ZTE5YQ=="){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -162,12 +160,12 @@ if ($result['visita'] === "armenia"){
     ';
 }
 
-if ($result['local'] === "albania"){
+if ($result['local'] === "mallorca"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9BbGJhbmlhX3ZzX0lzbGFuZGlhXzIwMjIwOTI3XzE2NjQyOTgzMzgwMTgvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvOEI4NjI4MzlCMEQ0RUZBMEQ4Mzc0Q0ZBN0QxMzg1OTI1MUNEMTA3MDdCRUJEQkNBQzM3MTA4QTJGQzc4NTA3RC9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTUxYzk3YTcxMmRlNDRjMzY5YzkzYjY0MTkwM2E5ZWZiJmtleTI9N2M4MTk4ZWY2NmVmNTg4MjgwM2IwYWYyZmVlZGM1MmY=";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
@@ -182,12 +180,12 @@ if ($result['local'] === "albania"){
     ';
 }
 
-if ($result['local'] === "kosovo"){
+if ($result['local'] === "PSG"){
     $canalImg = "starplus";
     // LINK:
-    $r= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9zL3N0YXJfandwLmh0bWw/Z2V0PWh0dHBzOi8vbGl2ZS1mdGMtbmEtc291dGgtMi5tZWRpYS5zdGFyb3R0LmNvbS9vbWEyL3ZhMDEvc3RhcnBsdXMvZXZlbnQvMjAyMi8wOS8yNy9Lb3Nvdm9fdnNfQ2hpcHJlXzIwMjIwOTI3XzE2NjQyOTgzNTEwMjEvY3RyLWFsbC1jb21wbGV0ZS5tM3U4JmltZz1odHRwczovL3Byb2QtcmlwY3V0LWRlbGl2ZXJ5LmRpc25leS1wbHVzLm5ldC92MS92YXJpYW50L3N0YXIvRjE1ODI3MkFDQ0UwNkYyQTY4QURFMEE0MTYxRjE4OTAzMTUyRkEyMjlGRTlBQkU5OUJGNzRCRjczOTc3OUNERS9zY2FsZT93aWR0aD0xOTIwJmFzcGVjdFJhdGlvPTEuNzgmZm9ybWF0PWpwZWcma2V5PTM3OTY3NzE5NDU1MDRmZDI4OGU5ZmNiZmY2NTY4NmNlJmtleTI9NzU2ZTM2ODk3YjFkZmY5YmQxYWYzZGUxNTEyMzYxZGU=";
+    $r= "";
 
-    if ($r !== ""){
+    if ($r === ""){
         $r = "../../../tv/epg?r=" . $r;
     } else {
         $r = "javascript:void(0)";
