c7f63ef7bfcb34e1b3d2f87c444777a7a42287ed
Alex <slowdsports@gmail.com>
2022-10-05 11:27:17 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -22,129 +22,11 @@ if ($result['local'] === "team"){
 
 if ($result['local'] === "salzburgo"){
     $canalImg = "starplus";
-    // LINK:
+    // STAR+:
     $r= "../../star.php";
 
-    if ($r !== ""){
-        $r = $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "realmadrid"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "../../star.php";
-
-    if ($r !== ""){
-        $r = $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "sevilla"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "../../star.php";
-
-    if ($r !== ""){
-        $r = $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "benfica"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "../../star.php";
-
-    if ($r !== ""){
-        $r = $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "chelsea"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "../../star.php";
-
-    if ($r !== ""){
-        $r = $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "man.city"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "../../star.php";
-
-    if ($r !== ""){
-        $r = $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "juventus"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "../../star.php";
-    $s = "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtRnJiUzVzWVhSaGJTNW9ZbTl0WVhoalpHNHVZMjl0TDJOdFlXWXZiR2wyWlM4eU1ETXhOakV4TDJSaGMyZ3ZZMlZ1WXk4eU56azNNUzl0WVhOMFpYSXViWEJrJmtleT1NREV3TVRVMk9XWmtPRGxpTnpZd1pEUmlNVEZoTm1JelpUWTRaR1ZrTnpnPSZrZXkyPU16TmtaV05oWkROaVl6RTJaRFEyTmpVeU9UUTRNVFl6WW1aa1kyVm1ZbVE9";
+    // HBO MAX:
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtRnJiUzVzWVhSaGJTNW9ZbTl0WVhoalpHNHVZMjl0TDJOdFlXWXZiR2wyWlM4eU1ETXhOakV4TDJSaGMyZ3ZZMlZ1WXk4eU56azNNUzl0WVhOMFpYSXViWEJrJmtleT1NREV3TVRVMk9XWmtPRGxpTnpZd1pEUmlNVEZoTm1JelpUWTRaR1ZrTnpnPSZrZXkyPU16TmtaV05oWkROaVl6RTJaRFEyTmpVeU9UUTRNVFl6WW1aa1kyVm1ZbVE9";
 
     if ($r !== ""){
         $r = $r;
@@ -161,13 +43,235 @@ if ($result['local'] === "juventus"){
     ';
 
     if ($s !== ""){
-        $s = $s;
+        $s = "../../../tv/epg?r=";
     } else {
         $s = "javascript:void(0)";
     }
     $custom2 = '
     <li>
-        <a class="justify-content-center" href="../../../tv/epg?r='.$s.'">
+        <a class="justify-content-center" href="'.$s.'">
+        <i class="flag ar"></i>
+        HBO MAX | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "realmadrid"){
+    $canalImg = "starplus";
+    // STAR+:
+    $r= "../../star.php";
+
+    // HBO MAX:
+    $s= "";
+
+    if ($r !== ""){
+        $r = $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+
+    if ($s !== ""){
+        $s = "../../../tv/epg?r=";
+    } else {
+        $s = "javascript:void(0)";
+    }
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$s.'">
+        <i class="flag ar"></i>
+        HBO MAX | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "sevilla"){
+    $canalImg = "starplus";
+    // STAR+:
+    $r= "../../star.php";
+
+    // HBO MAX:
+    $s= "";
+
+    if ($r !== ""){
+        $r = $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+
+    if ($s !== ""){
+        $s = "../../../tv/epg?r=";
+    } else {
+        $s = "javascript:void(0)";
+    }
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$s.'">
+        <i class="flag ar"></i>
+        HBO MAX | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "benfica"){
+    $canalImg = "starplus";
+    // STAR+:
+    $r= "../../star.php";
+
+    // HBO MAX:
+    $s= "";
+
+    if ($r !== ""){
+        $r = $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+
+    if ($s !== ""){
+        $s = "../../../tv/epg?r=";
+    } else {
+        $s = "javascript:void(0)";
+    }
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$s.'">
+        <i class="flag ar"></i>
+        HBO MAX | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "chelsea"){
+    $canalImg = "starplus";
+    // STAR+:
+    $r= "../../star.php";
+
+    // HBO MAX:
+    $s= "";
+
+    if ($r !== ""){
+        $r = $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+
+    if ($s !== ""){
+        $s = "../../../tv/epg?r=";
+    } else {
+        $s = "javascript:void(0)";
+    }
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$s.'">
+        <i class="flag ar"></i>
+        HBO MAX | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "man.city"){
+    $canalImg = "starplus";
+    // STAR+:
+    $r= "../../star.php";
+
+    // HBO MAX:
+    $s= "";
+
+    if ($r !== ""){
+        $r = $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+
+    if ($s !== ""){
+        $s = "../../../tv/epg?r=";
+    } else {
+        $s = "javascript:void(0)";
+    }
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$s.'">
+        <i class="flag ar"></i>
+        HBO MAX | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "juventus"){
+    $canalImg = "starplus";
+    // STAR+:
+    $r= "../../star.php";
+
+    // HBO MAX:
+    $s= "";
+
+    if ($r !== ""){
+        $r = $r;
+    } else {
+        $r = "javascript:void(0)";
+    }
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+
+    if ($s !== ""){
+        $s = "../../../tv/epg?r=";
+    } else {
+        $s = "javascript:void(0)";
+    }
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$s.'">
         <i class="flag ar"></i>
         HBO MAX | HD
         </a>
