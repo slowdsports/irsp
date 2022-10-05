c5ef7cd3464ae8774dc72cb005116a2f827c31b2
Alex <slowdsports@gmail.com>
2022-10-05 12:23:33 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -63,7 +63,7 @@ if ($result['local'] === "realmadrid"){
     $r= "../../star.php";
 
     // HBO MAX:
-    $s= "";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtTm1MbXhoZEdGdExtaGliMjFoZUdOa2JpNWpiMjB2WTIxaFppOXNhWFpsTHpJd016RTJNVEl2WkdGemFDOWpaVzVqTHpJM09UYzBMMjFoYzNSbGNpNXRjR1E9JmtleT1NREV3TVRGaE56STNOakJrWmpNMFlUSTVObU13TVRjelpXWmpZVFl6T0dJPSZrZXkyPU5tRmlNRFEwTkRNNFlUZGxZamswWlRNelpUSmtaVFZqWkdRM1pEZzBPR1k9";
 
     if ($r !== ""){
         $r = $r;
@@ -100,7 +100,7 @@ if ($result['local'] === "sevilla"){
     $r= "../../star.php";
 
     // HBO MAX:
-    $s= "";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtRnJiUzVzWVhSaGJTNW9ZbTl0WVhoalpHNHVZMjl0TDJOdFlXWXZiR2wyWlM4eU1ETXhOakUzTDJSaGMyZ3ZZMlZ1WXk4eU56azNPQzl0WVhOMFpYSXViWEJrJmtleT1NREV3TVdNd01tUTVPREEyTVRneU5ERXlPV1poWlRrMFpXVXlNakJrWmpJPSZrZXkyPU5EUXhORE5qWldSbU1UaGhNR1JtTmpReU5UZG1NVFF4WXprM09UazFOV009";
 
     if ($r !== ""){
         $r = $r;
@@ -137,7 +137,7 @@ if ($result['local'] === "benfica"){
     $r= "../../star.php";
 
     // HBO MAX:
-    $s= "";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtTm1MbXhoZEdGdExtaGliMjFoZUdOa2JpNWpiMjB2WTIxaFppOXNhWFpsTHpJd016RTJNVFV2WkdGemFDOWpaVzVqTHpJM09UZ3dMMjFoYzNSbGNpNXRjR1E9JmtleT1NREV3TVRGaFlUTXpNRGt5Wm1VeE9UUmlNR0ZrWXpBeU0ySm1OemRsWmpFPSZrZXkyPU5UZGtOR0prWkRreFlUa3hNVE01TkRKbVlURTVOalV5WXpaaVltRmxPREE9";
 
     if ($r !== ""){
         $r = $r;
@@ -174,7 +174,7 @@ if ($result['local'] === "chelsea"){
     $r= "../../star.php";
 
     // HBO MAX:
-    $s= "";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtTm1MbXhoZEdGdExtaGliMjFoZUdOa2JpNWpiMjB2WTIxaFppOXNhWFpsTHpJd016RTJNVFl2WkdGemFDOWpaVzVqTHpJM09UY3pMMjFoYzNSbGNpNXRjR1E9JmtleT1NREV3TVRaak1USmhPV1l4WWpZM1pHVTNaRGMyWW1JMk5EQmlObUV4TVRZPSZrZXkyPVlqTm1PRFkxTlRWalpHSXlZakprWldabE1EWm1ZV0ZoTnpoaFpUZ3lZV1E9";
 
     if ($r !== ""){
         $r = $r;
@@ -211,7 +211,7 @@ if ($result['local'] === "man.city"){
     $r= "../../star.php";
 
     // HBO MAX:
-    $s= "";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtWnNlUzVzWVhSaGJTNW9ZbTl0WVhoalpHNHVZMjl0TDJOdFlXWXZiR2wyWlM4eU1ETXhOamd6TDJSaGMyZ3ZZMlZ1WXk4eU56azNOUzl0WVhOMFpYSXViWEJrJmtleT1NREV3TVRSbVkyRmlPR0ZpTURCbFlUazROamMzT0RFMU1UTTBORGhpTkdVPSZrZXkyPU1URmtaak5qWlRrMllqVXlPR1F5WVRBMFpEUTBZekExWVRFME5UYzFZek09";
 
     if ($r !== ""){
         $r = $r;
@@ -248,7 +248,7 @@ if ($result['local'] === "juventus"){
     $r= "../../star.php";
 
     // HBO MAX:
-    $s= "";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtTm1MbXhoZEdGdExtaGliMjFoZUdOa2JpNWpiMjB2WTIxaFppOXNhWFpsTHpJd016RTJPREl2WkdGemFDOWpaVzVqTHpJM09UYzVMMjFoYzNSbGNpNXRjR1E9JmtleT1NREV3TVRJNVpqZ3dObVV3T1RaalpHUTRZV016TlRZelptTTJaVFEwWmpjPSZrZXkyPU1EWTBZalV6TmpCallqRTVPRE5rTTJOaE9URTVPR0ZpTVRjeVpEQTJNR1U9";
 
     if ($r !== ""){
         $r = $r;
