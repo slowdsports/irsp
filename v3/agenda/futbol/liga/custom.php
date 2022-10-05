c5f73e19ca97cf4ff179fd463c33849fa49eb572
Alex <slowdsports@gmail.com>
2022-10-05 10:57:21 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -144,6 +144,7 @@ if ($result['local'] === "juventus"){
     $canalImg = "starplus";
     // LINK:
     $r= "../../star.php";
+    $s = "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vandwLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtRnJiUzVzWVhSaGJTNW9ZbTl0WVhoalpHNHVZMjl0TDJOdFlXWXZiR2wyWlM4eU1ETXhOakV4TDJSaGMyZ3ZZMlZ1WXk4eU56azNNUzl0WVhOMFpYSXViWEJrJmtleT1NREV3TVRVMk9XWmtPRGxpTnpZd1pEUmlNVEZoTm1JelpUWTRaR1ZrTnpnPSZrZXkyPU16TmtaV05oWkROaVl6RTJaRFEyTmpVeU9UUTRNVFl6WW1aa1kyVm1ZbVE9";
 
     if ($r !== ""){
         $r = $r;
@@ -155,6 +156,20 @@ if ($result['local'] === "juventus"){
         <a class="justify-content-center" href="'.$r.'">
         <i class="flag ar"></i>
         Star + | HD
+        </a>
+    </li>
+    ';
+
+    if ($s !== ""){
+        $s = $s;
+    } else {
+        $s = "javascript:void(0)";
+    }
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$s.'">
+        <i class="flag ar"></i>
+        HBO MAX | HD
         </a>
     </li>
     ';
