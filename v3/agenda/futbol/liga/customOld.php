53cf041e6b62e991a7c8e0b84c59abcbc7f3f9fe
Alex <slowdsports@gmail.com>
2022-10-12 10:08:24 -0600

    Update customOld.php

--- v3/agenda/futbol/liga/customOld.php
+++ v3/agenda/futbol/liga/customOld.php
@@ -20,167 +20,7 @@ if ($result['local'] === "team"){
     ';
 }
 
-if ($result['visita'] === "chelsea"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['visita'] === "everton"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "fulham"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['visita'] === "brentford"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "inter"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "sevilla"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['visita'] === "wolves"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "mallorca"){
-    $canalImg = "starplus";
-    // LINK:
-    $r= "";
-
-    if ($r === ""){
-        $r = "../../../tv/epg?r=" . $r;
-    } else {
-        $r = "javascript:void(0)";
-    }
-    $custom1 = '
-    <li>
-        <a class="justify-content-center" href="'.$r.'">
-        <i class="flag ar"></i>
-        Star + | HD
-        </a>
-    </li>
-    ';
-}
-
-if ($result['local'] === "PSG"){
+if ($result['visita'] === "ajax" || $result['local'] === "at.madrid" || $result['local'] === "barcelona" || $result['visita'] === "liverpool" || $result['visita'] === "bayern" || $result['local'] === "tottenham" || $result['visita'] === "marseille" || $result['visita'] === "porto"){
     $canalImg = "starplus";
     // LINK:
     $r= "";
@@ -202,7 +42,98 @@ if ($result['local'] === "PSG"){
 
 
 //HBO
-if ($result['visita'] === "team"){
+if ($result['visita'] === "ajax"){
+    $canalImg = "hbow";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vYnRtLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtWnNlUzVzWVhSaGJTNW9ZbTl0WVhoalpHNHVZMjl0TDJOdFlXWXZiR2wyWlM4eU1ETXhOakV3TDJSaGMyZ3ZZMlZ1WXk4eU9ERTJOQzl0WVhOMFpYSXViWEJrJmtleT1NREV3TVRZNU1qSTBaR1ZsTjJaaVpqRmpaams1TURrd05tVmxNamxsWmpFPSZrZXkyPU16TmlNVEkwTVRVeVpXTTJZV1E1TmpKaFptUmtZVE5pWXpoak9ERTJObU09";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
+        <i class="flag ar"></i>
+        HBO Max | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "at.madrid"){
+    $canalImg = "hbow";
+    $s= "Ly92ZXJjb21vZnV0Ym9sLnh5ei9oYm8vYnRtLnBocD9nZXQ9YUhSMGNITTZMeTlzYVhabExtTm1MbXhoZEdGdExtaGliMjFoZUdOa2JpNWpiMjB2WTIxaFppOXNhWFpsTHpJd016RTJNVEV2WkdGemFDOWpaVzVqTHpJNE1UWXpMMjFoYzNSbGNpNXRjR1E9JmtleT1NREV3TVdRNFpEUTNNR1JtT0dVeFl6RXhPR1kyTkRobVpXWXhZakV5WkdVPSZrZXkyPVl6Um1aRGxsT1dRMU9HTTNObVk0WkRFMU1XTXpPV1prWkRKaU9EUXdZVEE9";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
+        <i class="flag ar"></i>
+        HBO Max | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "barcelona"){
+    $canalImg = "hbow";
+    $s= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
+        <i class="flag ar"></i>
+        HBO Max | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['visita'] === "liverpool"){
+    $canalImg = "hbow";
+    $s= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
+        <i class="flag ar"></i>
+        HBO Max | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['visita'] === "bayern"){
+    $canalImg = "hbow";
+    $s= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
+        <i class="flag ar"></i>
+        HBO Max | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['local'] === "tottenham"){
+    $canalImg = "hbow";
+    $s= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
+        <i class="flag ar"></i>
+        HBO Max | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['visita'] === "marseille"){
+    $canalImg = "hbow";
+    $s= "";
+    $custom1 = '
+    <li>
+        <a class="justify-content-center" href="../../../tv/epg?s='.$s.'">
+        <i class="flag ar"></i>
+        HBO Max | HD
+        </a>
+    </li>
+    ';
+}
+
+if ($result['visita'] === "porto"){
     $canalImg = "hbow";
     $s= "";
     $custom1 = '
