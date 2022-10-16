63973eaf3febfc1b7946fb52d476c3c9951199f4
Alex <slowdsports@gmail.com>
2022-10-16 00:38:58 -0600

    Update index.php

--- v3/agenda/nfl/index.php
+++ v3/agenda/nfl/index.php
@@ -125,7 +125,6 @@ if (isset($_SESSION['message']) ){
                                 include('channels.php');
                                 // Canal 2
                                 echo $canalop2;
-                                echo $canal2;
                                 // Canal 3
                                 echo $canalop3;
                                 // Canal 4
