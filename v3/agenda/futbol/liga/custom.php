bd1f43738e30b2dfd25f7e515fd902c73fe80201
Alex <slowdsports@gmail.com>
2022-10-17 22:18:26 -0600

    Update custom.php

--- v3/agenda/futbol/liga/custom.php
+++ v3/agenda/futbol/liga/custom.php
@@ -33,6 +33,19 @@ if ($result['local'] === "team"){
     ';
 }
 
+if ($result['canal3'] === "star+"){
+    $canalImg = "starplus";
+    $r = "../../star.php";
+    $custom2 = '
+    <li>
+        <a class="justify-content-center" href="'.$r.'">
+        <i class="flag ar"></i>
+        Star + | HD
+        </a>
+    </li>
+    ';
+}
+
 if ($result['local'] === "Balon De Oro" || $result['local'] === "lecce"){
     $canalImg = "starplus";
     // LINK:
