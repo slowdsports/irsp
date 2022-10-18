a3e7a979fb8415d0a28a8b13c36fe90c53c31f8a
Alex <slowdsports@gmail.com>
2022-10-17 21:05:58 -0600

    Update m3u.php

--- v3/tv/epg/valid/m3u.php
+++ v3/tv/epg/valid/m3u.php
@@ -1,22 +1,21 @@
 <?php
-// YouTube Poster
-if (strlen($result['channelUrl']) == 11){
-    //$iframeSrc = "//www.youtube.com/embed/" . $result['channelUrl'];
-    $iframeSrc = base64_encode($result['channelUrl']);
-    echo $iframeSrc;
-    $core = '<script src="cores/clappr.js"></script>';
+if ($typeChannel == 1 || $typeChannel == 8){
+    $core = '<script src="cores/jwp.js"></script>';
+    echo '<style>#iframe-element{display:none;} #player_img{display:none}</style>';
     echo '
-    <style>#jwp{display:block!important;} #iframe-element{display:none!important;} #player_img{display:none} #myTab{display:none}</style>
-    ';
-} else {
-    echo '<script>var ytposter = ""</script>';
-
-    if ($typeChannel == 1 || $typeChannel == 8){
-        $core = '<script src="cores/jwp.js"></script>';
-        echo '<style>#iframe-element{display:none;} #player_img{display:none}</style>';
+    <script>
+    var source = "'.$channel.'"
+    </script>';
+    // YouTube Poster
+    if (strlen($result['channelUrl']) == 11){
+        //$iframeSrc = "//www.youtube.com/embed/" . $result['channelUrl'];
+        $iframeSrc = base64_encode($result['channelUrl']);
+        echo $iframeSrc;
+        $core = '<script src="cores/clappr.js"></script>';
         echo '
-        <script>
-        var source = "'.$channel.'"
-        </script>';
+        <style>#jwp{display:block!important;} #iframe-element{display:none!important;} #player_img{display:none} #myTab{display:none}</style>
+        ';
+    } else {
+        echo '<script>var ytposter = ""</script>';
     }
 }
