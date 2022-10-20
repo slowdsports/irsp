d058892e6ead8dfd6145b379b9cbe53431500187
Alex <slowdsports@gmail.com>
2022-10-20 11:55:45 -0600

    Update channels.php

--- v3/agenda/basket/liga/channels.php
+++ v3/agenda/basket/liga/channels.php
@@ -13,7 +13,7 @@ if($canal === "M+ #Vamos" || $canal === "M+ #Vamos Bar 2 (305)" || $canal === "m
     </li>';
 } elseif($canal2 === "M+ #Vamos" || $canal2 === "M+ #Vamos Bar 2 (305)" || $canal2 === "mvamos"){
     $canalImg = "mvamos";
-    $canal2 = '
+    $canalop4 = '
     <li>
         <a class="justify-content-center" href="'.$app.'/tv/epg?futbol&id='.$index.'&c=mvamos">
         <i class="flag es"></i>
@@ -33,7 +33,7 @@ if($canal === "M+ Deportes" || $canal === "mdep"){
     </li>';
 } elseif($canal2 === "M+ Deportes" || $canal2 === "mdep"){
     $canalImg = "mdep";
-    $canal2 = '
+    $canalop4 = '
     <li>
         <a class="justify-content-center" href="'.$app.'/tv/epg?futbol&id='.$index.'&c=mdep">
         <i class="flag es"></i>
@@ -53,11 +53,31 @@ if($canal === "M+ Deportes 2" || $canal === "mdep2"){
     </li>';
 } elseif($canal2 === "M+ Deportes 2" || $canal2 === "mdep2"){
     $canalImg = "mdep";
-    $canal2 = '
+    $canalop4 = '
     <li>
         <a class="justify-content-center" href="'.$app.'/tv/epg?futbol&id='.$index.'&c=mdep2">
         <i class="flag es"></i>
         Movistar Deportes 2 | VIP HD
+        </a>
+    </li>';
+}
+
+if($canal === "DAZN3" || $canal === "DAZN3" || $canal === "dazn3"){
+    $canalImg = "dazn";
+    $canalop2 = '
+    <li>
+        <a class="justify-content-center" href="'.$app.'/tv/epg?futbol&id='.$index.'&c=dazn3">
+        <i class="flag es"></i>
+        DAZN 3 | VIP HD
+        </a>
+    </li>';
+} elseif($canal2 === "DAZN3" || $canal2 === "DAZN3" || $canal2 === "dazn3"){
+    $canalImg = "dazn";
+    $canalop4 = '
+    <li>
+        <a class="justify-content-center" href="'.$app.'/tv/epg?futbol&id='.$index.'&c=dazn3">
+        <i class="flag es"></i>
+        DAZN 3 | VIP HD
         </a>
     </li>';
 }
