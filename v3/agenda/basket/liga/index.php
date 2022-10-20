34fadfd9bdbc27f9658dcaa6cd811e0ad8a9ecd6
Alex <slowdsports@gmail.com>
2022-10-20 11:57:43 -0600

    Update index.php

--- v3/agenda/basket/liga/index.php
+++ v3/agenda/basket/liga/index.php
@@ -86,7 +86,6 @@ if (isset($_SESSION['message']) ){
                                 include('channels.php');
                                 // Canal 2
                                 echo $canalop2;
-                                echo $canal2;
                                 // Canal 3
                                 echo $canalop3;
                                 // Canal 4
