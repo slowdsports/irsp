825be7638be6a6fc80960b4d3536ff702a14ad2e
Alex <slowdsports@gmail.com>
2022-10-24 23:09:20 -0600

    Update index.php

--- v3/agenda/tenis/index.php
+++ v3/agenda/tenis/index.php
@@ -73,11 +73,13 @@ if (isset($_SESSION['message']) ){
                         <div class="card card-body">
                             <ul class="listview link-listview">
                                 <li>
-                                    <a class="justify-content-center" href="http://iraffle.live/v3/agenda/play/?c=mvamos">
+                                    <a class="justify-content-center" href="<?=$app?>/agenda/play/?c=mvamos">
                                         <i class="flag es"></i>
                                         #Vamos
                                     </a>
-                                    <a class="justify-content-center" href="http://iraffle.live/v3/agenda/play/?c=mdep">
+                                </li>
+                                <li>
+                                    <a class="justify-content-center" href="<?=$app?>/agenda/play/?c=mdep">
                                         <i class="flag es"></i>
                                         M+ Deportes
                                     </a>
