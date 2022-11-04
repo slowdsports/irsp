249f36f96bc304eecc070df4b3876f4ff35a3b10
Alex <slowdsports@gmail.com>
2022-11-03 23:39:48 -0600

    as

--- conn.php
+++ conn.php
@@ -1,10 +1,10 @@
 <?php
-//$conn = mysqli_connect("db5009304956.hosting-data.io","dbu2929559","FHZ]YvbV*8[/ugc$","dbs7886165"); //prod
-$conn = mysqli_connect("localhost","n57whzfm2yvo","SlowD2019","cookiename"); //local
+//$conn = mysqli_connect("localhost","n57whzfm2yvo","SlowD2019","cookiename"); //prod
+$conn = mysqli_connect("localhost","root","root","cookiename"); //local
 
 // Check connection
 if (mysqli_connect_errno())
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "¡Error al conectar a la base de datos: " . mysqli_connect_error();
   }
 ?>
